18d4073135ac94abf9601419d8f24f1e0cf4cdd6
yasunoh <checkupmaster@gmail.com>
2024-04-15 18:16:38 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// 管理用のルーティング
+Route::prefix('manage')->name('manage.')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Manage\TopController::class, 'index'])->name('home');
+    Route::get('/deals/search', [\App\Http\Controllers\Manage\DealsController::class, 'search'])->name('deals.search');
+    Route::resource('deals', \App\Http\Controllers\Manage\DealsController::class);
+    Route::resource('registers', \App\Http\Controllers\Manage\RegistersController::class);
+    Route::get('reserves/entry_date', [\App\Http\Controllers\Manage\ReservesController::class, 'entryDate'])->name('reserves.entry_date');
+    Route::get('reserves/entry_info', [\App\Http\Controllers\Manage\ReservesController::class, 'entryInfo'])->name('reserves.entry_info');
+    Route::get('reserves/confirm', [\App\Http\Controllers\Manage\ReservesController::class, 'confirm'])->name('reserves.confirm');
+    Route::resource('reserves', \App\Http\Controllers\Manage\ReservesController::class);
+
+
+});
+
+Route::get('reserves/entry_date', [\App\Http\Controllers\Member\ReservesController::class, 'entryDate'])->name('reserves.entry_date');
+Route::get('reserves/entry_info', [\App\Http\Controllers\Member\ReservesController::class, 'entryInfo'])->name('reserves.entry_info');
+Route::get('reserves/entry_car', [\App\Http\Controllers\Member\ReservesController::class, 'entryCar'])->name('reserves.entry_car');
+Route::get('reserves/option_select', [\App\Http\Controllers\Member\ReservesController::class, 'optionSelect'])->name('reserves.option_select');
+Route::get('reserves/confirm', [\App\Http\Controllers\Member\ReservesController::class, 'confirm'])->name('reserves.confirm');
+Route::get('reserves/complete', [\App\Http\Controllers\Member\ReservesController::class, 'complete'])->name('reserves.complete');
+Route::resource('reserves', \App\Http\Controllers\Member\ReservesController::class);
+
+
+
