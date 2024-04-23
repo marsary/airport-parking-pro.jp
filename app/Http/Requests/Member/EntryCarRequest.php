c0656e56f6641f37681586aaa4de8effe7b06ad3
yasunoh <checkupmaster@gmail.com>
2024-04-23 19:46:47 +0900

    EntryCarRequest

---
+++ app/Http/Requests/Member/EntryCarRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Member;
+
+use App\Rules\FlightNoDateRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EntryCarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'car_maker_id' => 'nullable',
+            'car_id' => 'nullable',
+            'car_color_id' => 'nullable',
+            'car_number' => 'nullable',
+            'flight_no' => ['nullable', new FlightNoDateRule],
+            'arrive_date' => 'nullable|date',
+            'num_members' => 'nullable|numeric|digits:4',
+        ];
+    }
+
+
+    public function attributes()
+    {
+        return [
+            'car_maker_id' => 'メーカー',
+            'car_id' => '車種',
+            'car_color_id' => '色',
+            'car_number' => 'ナンバー',
+            'flight_no' => '到着便',
+            'arrive_date' => '到着日',
+            'num_members' => 'ご利用人数',
+        ];
+    }
+}
---
+++ app/Rules/FlightNoDateRule.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\DB;
+
+class FlightNoDateRule implements ValidationRule, DataAwareRule
+{
+    /**
+     * バリデーション下の全データ
+     *
+     * @var array<string, mixed>
+     */
+    protected $data = [];
+
+    // ...
+
+    /**
+     * バリデーション下のデータをセット
+     *
+     * @param  array<string, mixed>  $data
+     */
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if(!isset($value) || !isset($this->data['arrive_date'])) {
+            return;
+        }
+        $exists = DB::table('arrival_flights')
+            ->where('flight_no', $value)
+            ->where('arrive_date', $this->data['arrive_date'])
+            ->exists();
+
+        if (!$exists) {
+            $fail('validation.flight_no_date')->translate();
+        }
+    }
+}
