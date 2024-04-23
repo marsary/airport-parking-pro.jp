68d06855909b78bb50486bf68d5ce0bd41aaa2d2
yasunoh <checkupmaster@gmail.com>
2024-04-23 19:50:11 +0900

    ReservesController 受付アクション追加

--- app/Http/Controllers/Member/ReservesController.php
+++ app/Http/Controllers/Member/ReservesController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Member\Forms\ReserveForm;
+use App\Http\Requests\Member\EntryCarRequest;
+use App\Http\Requests\Member\EntryDateRequest;
+use App\Http\Requests\Member\EntryInfoRequest;
+use App\Http\Requests\Member\OptionSelectRequest;
+use App\Models\CarMaker;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReservesController extends Controller
 {
@@ -14,52 +21,113 @@ class ReservesController extends Controller
     {
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-
-
     public function entryDate()
     {
-        return view('member.reserves.entry_date');
+        $reserve = $this->getReserveForm();
+
+        return view('member.reserves.entry_date', [
+            'reserve' => $reserve
+        ]);
+    }
+
+    public function postEntryDate(EntryDateRequest $request)
+    {
+        $reserve = $this->getReserveForm();
+        $reserve->fill($request->all());
+        session()->put('reserve', $reserve);
+        return redirect()->route('reserves.entry_info');
     }
 
     public function entryInfo()
     {
-        return view('member.reserves.entry_info');
+        $reserve = $this->getReserveForm();
+
+        return view('member.reserves.entry_info', [
+            'reserve' => $reserve
+        ]);
+    }
+
+    public function postEntryInfo(EntryInfoRequest $request)
+    {
+        $reserve = $this->getReserveForm();
+        $reserve->fill($request->all());
+        session()->put('reserve', $reserve);
+        return redirect()->route('reserves.entry_car');
     }
 
     public function entryCar()
     {
-        return view('member.reserves.entry_car');
+        $reserve = $this->getReserveForm();
+        $carMakers = CarMaker::select('name', 'id')->get();
+
+        return view('member.reserves.entry_car', [
+            'reserve' => $reserve,
+        ]);
+    }
+
+    public function postEntryCar(EntryCarRequest $request)
+    {
+        $reserve = $this->getReserveForm();
+        $reserve->fill($request->all());
+        session()->put('reserve', $reserve);
+        return redirect()->route('reserves.option_select');
     }
 
     public function optionSelect()
     {
-        return view('member.reserves.option_select');
+        $reserve = $this->getReserveForm();
+
+        return view('member.reserves.option_select', [
+            'reserve' => $reserve,
+        ]);
+    }
+
+    public function postOptionSelect(OptionSelectRequest $request)
+    {
+        $reserve = $this->getReserveForm();
+        $reserve->fill($request->all());
+        session()->put('reserve', $reserve);
+        return redirect()->route('reserves.confirm');
     }
 
     public function confirm()
     {
-        return view('member.reserves.confirm');
+        $reserve = $this->getReserveForm();
+
+        return view('member.reserves.confirm', [
+            'reserve' => $reserve,
+        ]);
     }
 
-    public function complete()
+
+    private function getReserveForm():ReserveForm
     {
-        return view('member.reserves.complete');
+        $reserve = session()->get('reserve');
+        if(!$reserve) {
+            $reserve = new ReserveForm();
+            $member = Auth::guard('members')->user();
+            $reserve->setMember($member);
+        }
+        return $reserve;
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $reserve = $this->getReserveForm();
+
+        return redirect()->route('reserves.complete');
+    }
+
+
+    public function complete(Request $request)
+    {
+        return view('member.reserves.complete', [
+            'reserveCode' => $request->code
+        ]);
     }
 
     /**
