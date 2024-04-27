fb40f985c1d96fb7ceda9b4f55d024bc258e5ef6
yasunoh <checkupmaster@gmail.com>
2024-04-27 20:05:42 +0900

    オプション入力画面続き

--- resources/views/include/option/option.blade.php
+++ resources/views/include/option/option.blade.php
@@ -1,17 +1,21 @@
   <!-- オプションをクリックしたら出てくるmodal -->
-  <div id="modalAreaOption" class="l-modal">
+  <div id="modalAreaOption{{$modalId}}" class="l-modal modal-option">
     <!-- モーダルのinnerを記述   -->
     <div class="l-modal__inner">
       <div class="l-modal__head">オプション選択：洗車</div>
-      <div class="l-modal__close modal_optionClose">×</div>
+      <div class="l-modal__close modal_optionClose" onclick="closeOptionModal({{$modalId}})">×</div>
       <div class="l-modal__content">
         <div class="l-grid--col4 l-grid--gap1">
           <!-- 洗車を押したら表示 -->
-          <div class="c-button-optionSelect">
-            <input type="checkbox" id="car_wash" name="car_wash" value="洗車">
-            <label for="car_wash" class="text-center u-pt2 u-pb2">手洗いWAX洗車<br>2，500円</label>
-          </div>
-          <div class="c-button-optionSelect">
+          @foreach ($goods as $good)
+            <div class="c-button-optionSelect">
+              <input type="checkbox" id="modal_good_ids_{{$good->id}}" name="modal_good_ids[]" value="{{$good->id}}"
+                {{(in_array($good->id, old('modal_good_ids', $reserve->good_ids))) ? 'checked ':''}}
+              >
+              <label for="modal_good_ids_{{$good->id}}" class="text-center u-pt2 u-pb2">{{$good->name}}<br>{{number_format($good->price)}}円</label>
+            </div>
+          @endforeach
+          {{--  <div class="c-button-optionSelect">
             <input type="checkbox" id="wax" name="wax" value="WAX">
             <label for="wax" class="text-center u-pt2 u-pb2">WAX<br>2，500円</label>
           </div>
@@ -30,12 +34,13 @@
           <div class="c-button-optionSelect">
             <input type="checkbox" id="dummy4" name="dummy4" value="">
             <label for="dummy4" class="text-center u-pt2 u-pb2">dummy4<br>2，500円</label>
-          </div>
+          </div>  --}}
         </div>
         <div class="l-flex--center l-grid--gap1 u-mt3">
-          <button type="button" class="c-button__submit--gray modal_optionClose">閉じる</button>
-          <button type="button" id="modal_add" class="c-button__submit--green">追加</button>
+          <button type="button" class="c-button__submit--gray modal_optionClose" onclick="closeOptionModal({{$modalId}})">閉じる</button>
+          <button type="button" id="modal_add_{{$modalId}}" onclick="addOptions({{$modalId}})" class="c-button__submit--green">追加</button>
         </div>
       </div>
     </div>
   </div>
+
--- resources/views/layouts/member/authenticated.blade.php
+++ resources/views/layouts/member/authenticated.blade.php
@@ -13,11 +13,11 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&display=swap" rel="preload" as="style" />
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'" />
+    {{--  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&display=swap" rel="preload" as="style" />
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'" />  --}}
 
     <!-- 電卓で使用 -->
-    <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet">
+    {{--  <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet">  --}}
 
     <!-- css -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
@@ -26,6 +26,7 @@
     <!-- <link rel="stylesheet" type='text/css' media='all' href="/css/webslides.css"> -->
     <!-- Optional - CSS SVG Icons (Font Awesome) -->
     <link rel="stylesheet" type='text/css' media='all' href="{{ asset('css/svg-icons.css') }}">
+    <link rel="stylesheet" type='text/css' href="{{ asset('css/app.css') }}">
     @stack('css')
 
     <!-- CSRF Token -->
@@ -34,6 +35,10 @@
     <!-- base path -->
     <input type="hidden" value="{{url('/')}}" id="base_path" name="base_path">
 
+    <!-- js -->
+    <script src="{{ asset('js/request.js') }}" defer></script>
+    <script src="{{ asset('js/luxon.min.js') }}" defer></script>
+    <script src="{{ asset('js/global.js') }}" defer></script>
   </head>
   <body>
     @yield('content')
