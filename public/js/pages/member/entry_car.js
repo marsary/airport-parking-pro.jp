f3f7c21b7472b98d0a26cdea31a574fd6e017f6b
yasunoh <checkupmaster@gmail.com>
2024-04-27 19:58:23 +0900

    entry_car.js作成

---
+++ public/js/pages/member/entry_car.js
@@ -0,0 +1,101 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const BASE_PATH = document.getElementById('base_path').value;
+  const flightNoElem = document.getElementById('flight_no');
+  const arriveDateElem = document.getElementById('arrive_date');
+  const airlineNameElem = document.getElementById('airline_name');
+  const depAirportNameElem = document.getElementById('dep_airport_name');
+  const arrAirportNameElem = document.getElementById('arr_airport_name');
+  const arriveTimeElem = document.getElementById('arrive_time');
+  const unloadDateElem = document.getElementById('unload_date_plan');
+
+  const carMakersElem = document.getElementById('car_maker_id');
+  const carsElem = document.getElementById('car_id');
+  const arrivalFlgElems = Array.from(document.getElementsByClassName('arrival_flg'));
+
+  flightNoElem.addEventListener('change', function() {
+    dispArrivalFlight()
+  });
+  arriveDateElem.addEventListener('change', function() {
+    dispArrivalFlight()
+    dispArrivalFlg()
+  });
+  carMakersElem.addEventListener('change', function() {
+    loadCars()
+  });
+
+  function dispArrivalFlg() {
+    const arriveDate = luxon.DateTime.fromSQL(arriveDateElem.value);
+    const unloadDate = luxon.DateTime.fromSQL(unloadDateElem.value);
+    if(!arriveDate.isValid || !unloadDate.isValid) {
+      return;
+    }
+    const isSameDate = arriveDate.hasSame(unloadDate, 'day')
+    arrivalFlgElems.forEach((flgElem) => {
+      if(isSameDate) {
+        flgElem.classList.add('hidden')
+      } else {
+        flgElem.classList.remove('hidden')
+      }
+    })
+  }
+
+  // 到着便情報の表示
+  async function dispArrivalFlight() {
+    const flightNo = flightNoElem.value;
+    const arriveDate = arriveDateElem.value;
+
+    if(flightNo == '' || arriveDate == '') {
+      return;
+    }
+
+    // 到着便・到着日をAPIに送信
+    const json = await apiRequest.get(BASE_PATH + "/arrival_flights/get_info",
+      {flight_no:flightNo, arrive_date:arriveDate}
+    )
+
+    console.log(json); // `data.json()` の呼び出しで解釈された JSON データ
+    if(json.success){
+      // 到着便情報の取得
+      console.log(json.data);
+      // ⑧	航空会社名 を表示
+      airlineNameElem.textContent = json.data.arrivalFlight.airlineName
+      // ⑨	出発空港 を表示
+      depAirportNameElem.textContent = json.data.arrivalFlight.depAirportName
+      // ⑩	到着空港 を表示
+      arrAirportNameElem.textContent = json.data.arrivalFlight.arrAirportName
+      // ⑪	到着予定時間 を表示
+      const loadTime = luxon.DateTime.fromISO(json.data.arrivalFlight.arriveTime);
+      if(loadTime.isValid) {
+        arriveTimeElem.textContent = loadTime.toFormat("HH:mm")
+      }
+    }
+  }
+
+  async function loadCars() {
+    // 選択メーカーIDをAPIに送信
+    // 車種リストの取得
+    const carMakerId = carMakersElem.value
+    const carId = carsElem.value
+    const json = await apiRequest.get(BASE_PATH + `/car_makers/${carMakerId}/cars`)
+    if(json.success){
+      while (carsElem.options.length > 1) carsElem.remove(1);
+        // car_id の select のオプションを更新する。
+        json.data.cars.forEach((car) => {
+            const option = document.createElement('option')
+            option.value = car.id;
+            option.textContent = car.name;
+            if(carId == car.id) {
+              option.selected = true
+            }
+            carsElem.appendChild(option)
+        });
+    }
+  }
+
+  // 初期表示
+  if(carsElem.value == '') {
+    loadCars()
+  }
+  dispArrivalFlight()
+  dispArrivalFlg()
+});
--- routes/web.php
+++ routes/web.php
@@ -28,14 +28,16 @@ Route::get('reserves/entry_date', [\App\Http\Controllers\Member\ReservesControll
 Route::get('reserves/entry_info', [\App\Http\Controllers\Member\ReservesController::class, 'entryInfo'])->name('reserves.entry_info');
 Route::get('reserves/entry_car', [\App\Http\Controllers\Member\ReservesController::class, 'entryCar'])->name('reserves.entry_car');
 Route::get('reserves/option_select', [\App\Http\Controllers\Member\ReservesController::class, 'optionSelect'])->name('reserves.option_select');
-Route::post('reserves/entry_date', [\App\Http\Controllers\Manage\ReservesController::class, 'postEntryDate'])->name('reserves.post_entry_date');
-Route::post('reserves/entry_info', [\App\Http\Controllers\Manage\ReservesController::class, 'postEntryInfo'])->name('reserves.post_entry_info');
+Route::post('reserves/entry_date', [\App\Http\Controllers\Member\ReservesController::class, 'postEntryDate'])->name('reserves.post_entry_date');
+Route::post('reserves/entry_info', [\App\Http\Controllers\Member\ReservesController::class, 'postEntryInfo'])->name('reserves.post_entry_info');
 Route::post('reserves/entry_car', [\App\Http\Controllers\Member\ReservesController::class, 'postEntryCar'])->name('reserves.post_entry_car');
 Route::post('reserves/option_select', [\App\Http\Controllers\Member\ReservesController::class, 'postOptionSelect'])->name('reserves.post_option_select');
 Route::get('reserves/confirm', [\App\Http\Controllers\Member\ReservesController::class, 'confirm'])->name('reserves.confirm');
 Route::get('reserves/complete', [\App\Http\Controllers\Member\ReservesController::class, 'complete'])->name('reserves.complete');
 Route::resource('reserves', \App\Http\Controllers\Member\ReservesController::class);
 
+Route::get('car_makers/{car_maker_id}/cars', [\App\Http\Controllers\CarMakersController::class, 'cars'])->name('car_makers.cars');
+Route::get('arrival_flights/get_info', [\App\Http\Controllers\ArrivalFlightsController::class, 'getInfo'])->name('arrival_flights.get_info');
 
 require __DIR__.'/manage_auth.php';
 require __DIR__.'/member_auth.php';
