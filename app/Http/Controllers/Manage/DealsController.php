6225025e9a082a55f86ea35165ecc5ac8aaebc56
yasunoh <checkupmaster@gmail.com>
2024-05-13 19:57:48 +0900

    DealsController 検索アクション

--- app/Http/Controllers/Manage/DealsController.php
+++ app/Http/Controllers/Manage/DealsController.php
@@ -2,7 +2,21 @@
 
 namespace App\Http\Controllers\Manage;
 
+use App\Enums\DealStatus;
+use App\Exports\DealSearchExport;
 use App\Http\Controllers\Manage\Controller;
+use App\Http\Requests\Manage\DealSearchRequest;
+use App\Models\Agency;
+use App\Models\Airline;
+use App\Models\Airport;
+use App\Models\Car;
+use App\Models\CarCaution;
+use App\Models\CarColor;
+use App\Models\CarMaker;
+use App\Models\Deal;
+use App\Services\LabelTagManager;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class DealsController extends Controller
@@ -10,18 +24,78 @@ class DealsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function search()
+    public function search(Request $request)
     {
-        return view('manage.deals.search');
+        $agencies = Agency::select('name', 'id')->get();
+        $carMakers = CarMaker::select('name', 'id')->get();
+        $cars = Car::select('name', 'id')->get();
+        $carColors = CarColor::select('name', 'id')->get();
+        $airports = Airport::select('name', 'id')->get();
+        $airlines = Airline::select('name', 'id')->get();
+        $carCautions = CarCaution::where('office_id', config('const.commons.office_id'))
+            ->select('name', 'id')->get();
+        $labels = LabelTagManager::getLabelTags(config('const.commons.office_id'));
+
+        return view('manage.deals.search', [
+            'agencies' => $agencies,
+            'carMakers' => $carMakers,
+            'cars' => $cars,
+            'carColors' => $carColors,
+            'airports' => $airports,
+            'airlines' => $airlines,
+            'carCautions' => $carCautions,
+            'labels' => $labels,
+        ]);
     }
 
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(DealSearchRequest $request)
     {
-        return view('manage.deals.list');
+        // dd($request->all());
+        $query = $this->getDealQueryFromRequest($request);
+        $deals = $query->with(['member', 'agency'])
+            ->orderBy('reserve_date', 'asc')
+            ->paginate()
+            ->withQueryString()
+            ;
+
+        return view('manage.deals.list', [
+            'deals' => $deals,
+        ]);
     }
+
+    private function getDealQueryFromRequest(Request $request):Builder
+    {
+        $today = Carbon::today();
+
+        $query = Deal::query();
+        // 予約ステータス
+        $query->when($request->input('reserved'), function($query, $search) {
+                $query->whereNot('status', DealStatus::CANCEL->value);
+            })
+            ->when($request->input('load_today'), function($query, $search)use($today){
+                $query->whereDate('load_date',$today);
+            })
+            ->when($request->input('loaded'), function($query, $search){
+                $query->where('status', DealStatus::LOADED->value);
+            })
+            ->when($request->input('unload_plan_today'), function($query, $search)use($today){
+                $query->whereDate('unload_date_plan',$today);
+            })
+            ->when($request->input('pending'), function($query, $search){
+                $query->where('status', DealStatus::PENDING->value);
+            })
+            ->when($request->input('unloaded'), function($query, $search){
+                $query->where('status', DealStatus::UNLOADED->value);
+            })
+            ;
+            // $query->dd();
+        return $query;
+    }
+
+
 
     /**
      * Show the form for creating a new resource.
