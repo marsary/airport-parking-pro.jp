5a318bde033388ff71cb63028bf3aa4058fd48c4
yasunoh <checkupmaster@gmail.com>
2024-05-14 20:03:23 +0900

    予約一覧用js関数追加

--- public/js/global.js
+++ public/js/global.js
@@ -12,3 +12,52 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
+
+function getToUrl(url = null, params = {}, withQueryStrings = true) {
+
+    url = url ? url + '?' : location.pathname + '?';
+    if(withQueryStrings) {
+        searchParams = new URLSearchParams(window.location.search);
+    } else {
+        searchParams = new URLSearchParams();
+    }
+    Object.keys(params).forEach((key) => {
+        if (Array.isArray(params[key])) {
+            searchParams.append(key, params[key].join(","));
+        } else {
+            searchParams.set(key, params[key])
+        }
+    })
+
+    location.href = url + searchParams.toString();
+};
+
+/**
+ *
+ * @param {HTMLTableElement} tableElem
+ * @param {string} checkboxName
+ * @returns
+ */
+function getTableCheckedRowValues(tableElem, checkboxName) {
+    let valueArray = [];
+
+    const checkedBoxes = tableElem.querySelectorAll('input[name=' + checkboxName + ']:checked');
+    checkedBoxes.forEach(checkbox => {
+        valueArray.push(checkbox.value)
+    });
+
+    return valueArray;
+}
+
+/**
+ *
+ * @param {HTMLTableElement} tableElem
+ * @param {string} checkboxName
+ * @param {boolean} shouldCheck
+ */
+function checkAllCheckboxes(tableElem, checkboxName, shouldCheck) {
+    const checkedBoxes = tableElem.querySelectorAll('input[name=' + checkboxName + ']');
+    checkedBoxes.forEach(checkbox => {
+        checkbox.checked = shouldCheck;
+    });
+}
