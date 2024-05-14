317f33b9f474dad724abb8f4b00864b4a53f159a
yasunoh <checkupmaster@gmail.com>
2024-05-14 19:58:49 +0900

    予約一覧アクションの作成

--- app/Http/Controllers/Manage/DealsController.php
+++ app/Http/Controllers/Manage/DealsController.php
@@ -36,6 +36,10 @@ class DealsController extends Controller
             ->select('name', 'id')->get();
         $labels = LabelTagManager::getLabelTags(config('const.commons.office_id'));
 
+        if(empty(session()->getOldInput())) {
+            session()->flash('_old_input', $request->all());
+        }
+
         return view('manage.deals.search', [
             'agencies' => $agencies,
             'carMakers' => $carMakers,
@@ -57,7 +61,7 @@ class DealsController extends Controller
         $query = $this->getDealQueryFromRequest($request);
         $deals = $query->with(['member', 'agency'])
             ->orderBy('reserve_date', 'asc')
-            ->paginate()
+            ->paginate($request->input('limit') ?? 25)
             ->withQueryString()
             ;
 
@@ -89,6 +93,195 @@ class DealsController extends Controller
             })
             ->when($request->input('unloaded'), function($query, $search){
                 $query->where('status', DealStatus::UNLOADED->value);
+            })
+            // 予約情報
+            ->when($request->input('reserve_code'), function($query, $search){
+                $query->where('reserve_code', $search);
+            })
+            ->when($request->input('receipt_code'), function($query, $search){
+                $query->where('receipt_code', $search);
+            })
+            ->when($request->input('reserve_date'), function($query, $search){
+                $datetime = Carbon::parse($search);
+                $query->whereDate('reserve_date',$datetime);
+            })
+            ->when($request->input('agency_id'), function($query, $search){
+                $query->where('agency_id', $search);
+            })
+            ->when($request->input('load_date_start'), function($query, $search){
+                $datetime = Carbon::parse($search)->toDateString();
+                $query->where('load_date','>=', $datetime);
+            })
+            ->when($request->input('load_date_end'), function($query, $search){
+                $datetime = Carbon::parse($search)->toDateString();
+                $query->where('load_date','<=', $datetime);
+            })
+            ->when($request->input('load_time_start'), function($query, $search){
+                $query->whereTime('load_time','>=', $search);
+            })
+            ->when($request->input('load_time_end'), function($query, $search){
+                $query->whereTime('load_time','<=', $search);
+            })
+            ->when($request->input('unload_date_plan_start'), function($query, $search){
+                $datetime = Carbon::parse($search)->toDateString();
+                $query->where('unload_date_plan','>=', $datetime);
+            })
+            ->when($request->input('unload_date_plan_end'), function($query, $search){
+                $datetime = Carbon::parse($search)->toDateString();
+                $query->where('unload_date_plan','<=', $datetime);
+            })
+            ->when($request->input('unload_date_start'), function($query, $search){
+                $datetime = Carbon::parse($search)->toDateString();
+                $query->where('unload_date','>=', $datetime);
+            })
+            ->when($request->input('unload_date_end'), function($query, $search){
+                $datetime = Carbon::parse($search)->toDateString();
+                $query->where('unload_date','<=', $datetime);
+            })
+            ->when($request->input('num_days_start'), function($query, $search){
+                $query->where('num_days','>=', $search);
+            })
+            ->when($request->input('num_days_end'), function($query, $search){
+                $query->where('num_days','<=', $search);
+            })
+            // 顧客情報
+            ->when($request->input('member_code'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('member_code', $search);
+                });
+            })
+            ->when($request->input('name'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('name', $search);
+                });
+            })
+            ->when($request->input('kana'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('kana', $search);
+                });
+            })
+            ->when($request->input('used_num'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('used_num', $search);
+                });
+            })
+            ->when($request->input('label_tag'), function($query, $search){
+                foreach ($search as $labelId => $tagId) {
+                    $query->whereHas('member.tagMembers', function (Builder $query) use($labelId, $tagId){
+                        $query->where('label_id', $labelId)
+                            ->where('tag_id', $tagId);
+                    });
+                }
+
+            })
+            ->when($request->input('zip'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('zip', $search);
+                });
+            })
+            ->when($request->input('tel'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('tel', $search);
+                });
+            })
+            ->when($request->input('email'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('email', $search);
+                });
+            })
+            ->when($request->input('line_id'), function($query, $search){
+                $query->whereHas('member', function (Builder $query) use($search){
+                    $query->where('line_id', $search);
+                });
+            })
+            ->when($request->input('receipt_address'), function($query, $search){
+                $query->where('receipt_address', $search);
+            })
+            // 到着予定
+            ->when($request->input('arrive_date_start'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $datetime = Carbon::parse($search)->toDateString();
+                    $query->where('arrive_date','>=', $datetime);
+                });
+            })
+            ->when($request->input('arrive_date_end'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $datetime = Carbon::parse($search)->toDateString();
+                    $query->where('arrive_date','<=', $datetime);
+                });
+            })
+            ->when($request->input('arrive_time_start'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->whereTime('arrive_time','>=', $search);
+                });
+            })
+            ->when($request->input('arrive_time_end'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->whereTime('arrive_time','<=', $search);
+                });
+            })
+            ->when($request->input('arrival_flight_name'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->where('flight_no', $search);
+                });
+            })
+            ->when($request->input('airline_id'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->where('airline_id', $search);
+                });
+            })
+            ->when($request->input('dep_airport_id'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->where('dep_airport_id', $search);
+                });
+            })
+            ->when($request->input('arr_airport_id'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->where('arr_airport_id', $search);
+                });
+            })
+            ->when($request->input('terminal_id'), function($query, $search){
+                $query->whereHas('arrivalFlight', function (Builder $query) use($search){
+                    $query->where('terminal_id', $search);
+                });
+            })
+            ->when($request->input('arrival_flg'), function($query, $search){
+                $query->where('arrival_flg', $search);
+            })
+            // 車両情報
+            ->when($request->input('car_maker_id'), function($query, $search){
+                $query->whereHas('memberCar.car', function (Builder $query) use($search){
+                    $query->where('car_maker_id', $search);
+                });
+            })
+            ->when($request->input('car_id'), function($query, $search){
+                $query->whereHas('memberCar', function (Builder $query) use($search){
+                    $query->where('car_id', $search);
+                });
+            })
+            ->when($request->input('car_color_id'), function($query, $search){
+                $query->whereHas('memberCar', function (Builder $query) use($search){
+                    $query->where('car_color_id', $search);
+                });
+            })
+            ->when($request->input('number'), function($query, $search){
+                $query->whereHas('memberCar', function (Builder $query) use($search){
+                    $query->where('number', $search);
+                });
+            })
+            ->when($request->input('size_type'), function($query, $search){
+                $query->whereHas('memberCar.car', function (Builder $query) use($search){
+                    $query->where('size_type', $search);
+                });
+            })
+            ->when($request->input('num_members'), function($query, $search){
+                $query->where('num_members', $search);
+            })
+            // 車両取扱
+            ->when($request->input('car_caution_id'), function($query, $search){
+                $query->whereHas('carCautionMemberCars', function (Builder $query) use($search){
+                    $query->where('car_caution_id', $search);
+                });
             })
             ;
             // $query->dd();
