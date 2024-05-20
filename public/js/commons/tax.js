4aa9c158360e55b1ccd96d465dc683bd7844f2f7
yasunoh <checkupmaster@gmail.com>
2024-05-20 20:06:03 +0900

    tax.js

---
+++ public/js/commons/tax.js
@@ -0,0 +1,22 @@
+const TaxTypes = Object.freeze({
+    EIGHT_PERCENT: 1,
+    TEN_PERCENT: 2,
+    EXEMPT: 3
+});
+
+function calcTax(taxType, price = null) {
+    switch (taxType) {
+        case TaxTypes.EIGHT_PERCENT:
+            return parseInt(price * 0.08);
+            break;
+        case TaxTypes.TEN_PERCENT:
+            return parseInt(price * 0.1);
+            break;
+        case TaxTypes.EXEMPT:
+            return price;
+            break;
+        default:
+            return price
+            break;
+    }
+}
