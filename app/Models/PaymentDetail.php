d478de2dbbf7795535251fdc6a93cf340a9a11cb
yasunoh <checkupmaster@gmail.com>
2024-05-25 19:43:32 +0900

    モデルの作成

---
+++ app/Models/PaymentDetail.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentDetail extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'payment_id',
+        'payment_method_id',
+        'total_price',
+    ];
+
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
+}
---
+++ app/Models/PaymentGood.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PaymentGood extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'payment_id',
+        'good_category_id',
+        'deal_good_id',
+        'name',
+        'price',
+        'tax',
+        'tax_type',
+        'num',
+        'total_price',
+        'total_tax',
+        'created_by',
+        'updated_by',
+    ];
+}
