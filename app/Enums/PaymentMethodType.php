5b1d738dc07b27eba5a6ffef1a694b7f7f3cde97
yasunoh <checkupmaster@gmail.com>
2024-05-27 20:07:47 +0900

    PaymentMethodType修正

--- app/Enums/PaymentMethodType.php
+++ app/Enums/PaymentMethodType.php
@@ -5,7 +5,8 @@ namespace App\Enums;
 enum PaymentMethodType: int
 {
     // 支払方法マスタ区分
-    // 1：現金、2：クレジット、３：電子マネー、４：QRコード、５：商品券、６：旅行支援、７：バウチャー、８：その他
+    // 1：現金、2：クレジット、３：電子マネー、４：QRコード、５：商品券、
+    // ６：旅行支援、７：バウチャー、８：その他、９：値引き、１０：調整
 
     // 基本情報
     case CASH = 1;
@@ -16,6 +17,8 @@ enum PaymentMethodType: int
     case TRAVEL_ASSIST = 6;
     case VOUCHER = 7;
     case OTHER = 8;
+    case DISCOUNT = 9;
+    case ADJUSTMENT = 10;
 
     // 日本語を追加
     public function label(): string
@@ -30,6 +33,42 @@ enum PaymentMethodType: int
             PaymentMethodType::TRAVEL_ASSIST => '旅行支援',
             PaymentMethodType::VOUCHER => 'バウチャー',
             PaymentMethodType::OTHER => 'その他',
+            PaymentMethodType::DISCOUNT => '値引き',
+            PaymentMethodType::ADJUSTMENT => '調整',
+        };
+    }
+
+    public function symbol(): string
+    {
+        return match($this)
+        {
+            PaymentMethodType::CASH => 'cash',
+            PaymentMethodType::CREDIT => 'credit',
+            PaymentMethodType::E_MONEY => 'electronicMoney',
+            PaymentMethodType::QR_CODE => 'qrCode',
+            PaymentMethodType::GIFT_CERTIFICATE => 'giftCertificates',
+            PaymentMethodType::TRAVEL_ASSIST => 'travelAssistance',
+            PaymentMethodType::VOUCHER => 'voucher',
+            PaymentMethodType::OTHER => 'others',
+            PaymentMethodType::DISCOUNT => 'discount',
+            PaymentMethodType::ADJUSTMENT => 'adjustment',
+        };
+    }
+
+    public function symbolToId(): int
+    {
+        return match($this)
+        {
+            'cash' => PaymentMethodType::CASH->value,
+            'credit' => PaymentMethodType::CREDIT->value,
+            'electronicMoney' => PaymentMethodType::E_MONEY->value,
+            'qrCode' => PaymentMethodType::QR_CODE->value,
+            'giftCertificates' => PaymentMethodType::GIFT_CERTIFICATE->value,
+            'travelAssistance' => PaymentMethodType::TRAVEL_ASSIST->value,
+            'voucher' => PaymentMethodType::VOUCHER->value,
+            'others' => PaymentMethodType::OTHER->value,
+            'discount' => PaymentMethodType::DISCOUNT->value,
+            'adjustment' => PaymentMethodType::ADJUSTMENT->value,
         };
     }
 }
