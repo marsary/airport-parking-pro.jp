3cc12a7afe90494ce91321d0907bcd5e563da824
yasunoh <checkupmaster@gmail.com>
2024-06-10 17:53:44 +0900

    Controller 修正

--- app/Http/Controllers/Manage/Controller.php
+++ app/Http/Controllers/Manage/Controller.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Manage;
 
+use App\Http\Controllers\Manage\Commons\HeaderInfo;
 
 abstract class Controller
 {
+    use HeaderInfo;
+
     function __construct()
     {
-
+        $this->shareHeaderInfo();
     }
 }
