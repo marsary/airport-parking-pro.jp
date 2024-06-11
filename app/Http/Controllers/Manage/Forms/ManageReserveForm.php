d7030fd0e02861434959ab6fcd5e30a6877769b4
yasunoh <checkupmaster@gmail.com>
2024-06-11 18:01:15 +0900

    ManageReserveForm

---
+++ app/Http/Controllers/Manage/Forms/ManageReserveForm.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers\Manage\Forms;
+
+use App\Http\Controllers\Forms\ReserveFormBase;
+use App\Models\CarCaution;
+use App\Models\Member;
+use Illuminate\Support\Facades\DB;
+
+class ManageReserveForm extends ReserveFormBase
+{
+    public $car_caution_ids = [];
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function setMember(Member $member = null)
+    {
+        parent::setMember($member);
+
+        if($this->member_car_id) {
+            $this->car_caution_ids = DB::table('car_cautions')
+                ->leftJoin('car_caution_member_cars', 'car_cautions.id', '=', 'car_caution_member_cars.car_caution_id')
+                ->where('car_caution_member_cars.member_car_id', $this->member_car_id)
+                ->pluck('car_cautions.id')->toArray();
+        }
+    }
+
+    public function handleGoodsAndTotals()
+    {
+        parent::handleGoodsAndTotals();
+
+        // TODO クーポンの処理
+    }
+}
