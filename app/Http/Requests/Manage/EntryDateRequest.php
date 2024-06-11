1ded4c0d2a9ac9b5d22428633342f58e7f7dfaf3
yasunoh <checkupmaster@gmail.com>
2024-06-11 18:01:23 +0900

    EntryDateRequest

---
+++ app/Http/Requests/Manage/EntryDateRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Requests\Manage;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\DB;
+
+class EntryDateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'agency_code' => 'nullable|string|exists:agencies,code|max:100',
+            'agency_id' => 'nullable|int',
+            'load_date' => 'required|date',
+            'load_time' => 'required|date_format:G:i',
+            'unload_date_plan' => 'required|date|after:load_date',
+            'unload_time_plan' => 'nullable|date_format:H:i',
+            'num_days' => 'required|int',
+            'coupon_ids' => 'nullable|array',
+            'coupon_ids.*' => 'int',
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        if(isset($this->agency_code)) {
+            $agency = DB::table('agencies')->where('code', $this->agency_code)->first();
+        }
+        $this->merge([
+            'agency_id' => isset($agency) ? $agency->id: null,
+        ]);
+        $coupon_ids = [];
+        if(!empty($this->coupon_ids)) {
+            $coupon_ids = explode(',', $this->coupon_ids);
+        }
+        $this->merge([
+            'coupon_ids' => $coupon_ids,
+        ]);
+    }
+
+    public function attributes()
+    {
+        return [
+            'agency_code' => '代理店ID',
+            'load_date' => '入庫日',
+            'load_time' => '入庫時間',
+            'unload_date_plan' => '出庫予定日',
+            'unload_time_plan' => '出庫予定時間',
+            'num_days' => '利用日数',
+            'coupon_ids' => 'クーポン選択',
+        ];
+    }
+}
