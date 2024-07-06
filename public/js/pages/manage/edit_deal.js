9b5eb108f857b6f45d6454d1214ec2df89629a34
yasunoh <checkupmaster@gmail.com>
2024-07-06 17:51:26 +0900

    entry_deal.js 処理実装

---
+++ public/js/pages/manage/edit_deal.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const BASE_PATH = document.getElementById('base_path').value;
+  const carCautionSelect = document.getElementById('car_caution_ids');
+  const carMakersElem = document.getElementById('car_maker_id');
+  const carsElem = document.getElementById('car_id');
+  const loadDateInput = document.getElementById('load_date')
+  const loadTimeInput = document.getElementById('load_time')
+  const unloadDatePlanInput = document.getElementById('unload_date_plan')
+  const dealId = document.getElementById('deal_id').value
+
+  $(carCautionSelect).select2();
+
+  carMakersElem.addEventListener('change', function() {
+    loadCars()
+  });
+
+  loadDateInput.addEventListener('click', function() {
+    location.href = BASE_PATH + `/manage/deals/${dealId}/entry_date`
+  });
+  loadTimeInput.addEventListener('click', function() {
+    location.href = BASE_PATH + `/manage/deals/${dealId}/entry_date`
+  });
+  unloadDatePlanInput.addEventListener('click', function() {
+    location.href = BASE_PATH + `/manage/deals/${dealId}/entry_date`
+  });
+
+  async function loadCars() {
+    // 選択メーカーIDをAPIに送信
+    // 車種リストの取得
+    const carMakerId = carMakersElem.value
+    const carId = carsElem.value
+    const json = await apiRequest.get(BASE_PATH + `/car_makers/${carMakerId}/cars`)
+    if(json.success){
+      while (carsElem.options.length > 1) carsElem.remove(1);
+        // car_id の select のオプションを更新する。
+        json.data.cars.forEach((car) => {
+            const option = document.createElement('option')
+            option.value = car.id;
+            option.dataset.size = car.size_type;
+            option.textContent = car.name;
+            if(carId == car.id) {
+              option.selected = true
+            }
+            carsElem.appendChild(option)
+        });
+    }
+  }
+
+
+  // 初期表示
+  if(carsElem.value == '') {
+    loadCars()
+  }
+});
