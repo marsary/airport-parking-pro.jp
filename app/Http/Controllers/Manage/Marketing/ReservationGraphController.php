dcf88d704b3de487bcfd0896deecde493f39f1e3
yasunoh <checkupmaster@gmail.com>
2024-07-16 17:55:16 +0900

    ReservationGraphController続き

--- app/Http/Controllers/Manage/Marketing/ReservationGraphController.php
+++ app/Http/Controllers/Manage/Marketing/ReservationGraphController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Manage\Marketing;
 
+use App\Enums\ReservationRoute;
 use App\Http\Controllers\Manage\Controller;
+use App\Models\Deal;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReservationGraphController extends Controller
 {
@@ -37,8 +40,95 @@ class ReservationGraphController extends Controller
             'data' => [
                 'datasets' => $data,
                 'labels' => $labels,
+                'currentDate' => $startDate,
             ]
          ]);
     }
 
+
+    public function chartByDay(Request $request)
+    {
+        [$startDate, $endDate] = $this->getStartEndDateFromView($request);
+        $period = CarbonPeriod::create($startDate, $endDate);
+        $labels = array_map(function($date){
+            return $date->format("Y-m-d");
+        }, $period->toArray());
+
+        $data = [];
+        // 取引　※電話予約（自）
+        $data[ReservationRoute::SELF_PHONE->label()] = $this->countByDay($startDate, $endDate, $period, ReservationRoute::SELF_PHONE);
+        // 取引　※電話予約（代）
+        $data[ReservationRoute::STAFF_PHONE->label()] = $this->countByDay($startDate, $endDate, $period, ReservationRoute::STAFF_PHONE);
+        // 取引　※当日受付予約
+        $data[ReservationRoute::COUNTER->label()] = $this->countByDay($startDate, $endDate, $period, ReservationRoute::COUNTER);
+        // 取引　※ネット予約（自）
+        $data[ReservationRoute::SELF_WEB->label()] = $this->countByDay($startDate, $endDate, $period, ReservationRoute::SELF_WEB);
+        // 取引　※ネット予約（代）
+        $data[ReservationRoute::STAFF_WEB->label()] = $this->countByDay($startDate, $endDate, $period, ReservationRoute::STAFF_WEB);
+
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'datasets' => $data,
+                'labels' => $labels,
+            ]
+         ]);
+    }
+
+    private function countByDay($startDate, $endDate, $period, ReservationRoute $route)
+    {
+        foreach($period as $date){
+            $range[$date->format("Y-m-d")] = 0;
+        }
+
+        $counts = Deal::select([
+            DB::raw('DATE(reserve_date) as reserve_date'),
+            DB::raw('COUNT(id) AS count'),
+        ])
+            ->where('office_id', config('const.commons.office_id'))
+            ->whereDate('reserve_date','>=', $startDate->toDateString())
+            ->whereDate('reserve_date','<=', $endDate->toDateString())
+            ->where('reservation_route', $route->value)
+            ->groupBy('reserve_date')
+            ->orderBy('reserve_date', 'ASC')
+            ->get();
+
+        foreach($counts as $countRow){
+            $range[$countRow->reserve_date->format("Y-m-d")] = $countRow->count;
+        }
+        return $range;
+    }
+
+    private function getStartEndDateFromView(Request $request)
+    {
+        switch ($request->view) {
+            case 'monthly':
+                $startDate = Carbon::parse($request->currentStartDate);
+                if($request->has('nextPrev')) {
+                    if($request->nextPrev == 'next') {
+                        $startDate->addMonth();
+                    } elseif($request->nextPrev == 'prev') {
+                        $startDate->subMonth();
+                    }
+                }
+                $endDate = (clone $startDate)->endOfMonth();
+                break;
+            case 'weekly':
+                $startDate = Carbon::parse($request->currentStartDate);
+                if($request->nextPrev == 'next') {
+                    $startDate->addWeek();
+                } elseif($request->nextPrev == 'prev') {
+                    $startDate->subWeek();
+                }
+                $endDate = (clone $startDate)->endOfWeek();
+                break;
+            case 'manual':
+                $startDate = Carbon::parse($request->startDate);
+                $endDate = Carbon::parse($request->endDate);
+                break;
+        }
+
+        return [$startDate, $endDate];
+    }
 }
