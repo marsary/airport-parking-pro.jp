2a9e725a09a87b77259a1fad8f2a1e4440d4a817
yasunoh <checkupmaster@gmail.com>
2024-08-10 17:51:35 +0900

    InventoriesController作成

---
+++ app/Http/Controllers/Manage/Ledger/InventoriesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Manage\Ledger;
+
+use App\Enums\DealStatus;
+use App\Http\Controllers\Manage\Controller;
+use App\Models\Deal;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class InventoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function inventories(Request $request)
+    {
+        // dd($request->all());
+        $dispLoadedUnloaded = $request->has('disp_loaded_unloaded') ? true:false;
+
+        $today = Carbon::today();
+        // 本日入庫一覧
+        $query = Deal::query()->whereDate('load_date',$today);
+        if($dispLoadedUnloaded) {
+            $query->whereIn('status', [
+                DealStatus::NOT_LOADED->value,
+                DealStatus::LOADED->value,
+            ]);
+        } else {
+            $query->whereIn('status', [
+                DealStatus::NOT_LOADED->value,
+            ]);
+        }
+        $loadDeals = $query->with(['member', 'arrivalFlight', 'memberCar'])
+            ->orderBy('load_time', 'asc')
+            ->get()
+            ;
+
+        return view('manage.ledger.inventories', [
+            'loadDeals' => $loadDeals,
+        ]);
+    }
+}
