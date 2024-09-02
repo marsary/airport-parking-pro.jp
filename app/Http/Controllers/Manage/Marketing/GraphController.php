924bf148ac1a95516661386f2737d6409e262d35
yasunoh <checkupmaster@gmail.com>
2024-09-02 17:13:58 +0900

    update GraphController

--- app/Http/Controllers/Manage/Marketing/GraphController.php
+++ app/Http/Controllers/Manage/Marketing/GraphController.php
@@ -3,15 +3,39 @@
 namespace App\Http\Controllers\Manage\Marketing;
 
 use App\Http\Controllers\Manage\Controller;
+use App\Services\Graphs\InventoryGraphs;
 use Illuminate\Http\Request;
 
 class GraphController extends Controller
 {
-    //
     public function inventory()
     {
         return view('manage.marketing.graph.inventory');
     }
+
+
+    public function chartByHour(Request $request, InventoryGraphs $graphService)
+    {
+        $data = $graphService->loadDataByHour($request);
+
+        return response()->json([
+            'success' => true,
+            'data' => $data
+         ]);
+    }
+
+
+
+    public function chartByDay(Request $request, InventoryGraphs $graphService)
+    {
+        $data = $graphService->loadDataByDay($request);
+
+        return response()->json([
+            'success' => true,
+            'data' => $data
+         ]);
+    }
+
 
     public function reservation()
     {
