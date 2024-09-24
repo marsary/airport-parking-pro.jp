896687bc0e1f35bf7344fc93d6a841f7ba2fdeb6
yasunoh <checkupmaster@gmail.com>
2024-09-24 17:45:43 +0900

    option_select.js リファクタリング等

--- public/js/pages/member/option_select.js
+++ public/js/pages/member/option_select.js
@@ -1,4 +1,4 @@
-// 合計金額を更新
+// モーダルの合計金額を更新
 function updateTotalAmount(input) {
     const modalAreaOption = input.closest('.modal-option');
     const inputs = modalAreaOption.querySelectorAll('.c-buttonQuantity__input');
@@ -13,6 +13,13 @@ function updateTotalAmount(input) {
     modalAreaOption.querySelector('.total-amount').textContent = totalAmount;
   }
 
+  /**
+   * 配列をオブジェクトに変換する
+   * オブジェクトの場合はそのまま返す
+   *
+   * @param {Array} value
+   * @returns object
+   */
   function asObject(value = []) {
     if(value != null && value.constructor.name === "Object") {
         return value;
