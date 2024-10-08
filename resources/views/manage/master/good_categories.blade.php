47f4133eb76213b31e758d2327ea1c5c128416c8
yasunoh <checkupmaster@gmail.com>
2024-10-08 17:58:10 +0900

    画面テンプレート修正

--- resources/views/manage/master/good_categories.blade.php
+++ resources/views/manage/master/good_categories.blade.php
@@ -11,23 +11,23 @@
 
       <div class="l-container__inner">
         <h2 class="c-title__lv2 l-flex--sb u-w-full-wide">新規登録 および 検索<span class="close_button c-button__close">閉じる</span></h2>
-        <form method="" action="/" class="u-mb4 u-font--md is-active">
+        <form method="GET" action="{{route('manage.master.good_categories.index')}}" class="u-mb4 u-font--md is-active">
           <div class="l-grid--col2-auto l-grid--gap2">
             <div>
-              <label for="category">商品カテゴリ名</label>
-              <input type="text" id="category" name="category" class="c-form-input--w100 u-mb0 u-h38">
+              <label for="name">商品カテゴリ名</label>
+              <input type="text" id="name" name="name" value="{{request('name')}}" class="c-form-input--w100 u-mb0 u-h38">
             </div>
             <div class="u-font--normal">
               <label for="division" class="u-font--md">区分</label>
               <!--  「出庫までに作業が必要」「出庫までに作業が不要」　radioボタン  -->
               <div class="c-button-optionSelect l-grid--col3 l-grid--gap1">
                 <div>
-                  <input type="radio" name="division" value="division1" id="division1" checked>
-                  <label for="division1" class="u-h38 u-mb0">出庫までに作業が必要</label>
+                  <input type="radio" name="type" value="1" id="type1" {{request('type') == 1 ? "checked":""}}>
+                  <label for="type1" class="u-h38 u-mb0">出庫までに作業が必要</label>
                 </div>
                 <div>
-                  <input type="radio" name="division" value="division2" id="division2">
-                  <label for="division2" class="u-h38 u-mb0">出庫までに作業が不要</label>
+                  <input type="radio" name="type" value="2" id="type2" {{request('type') == 2 ? "checked":""}}>
+                  <label for="type2" class="u-h38 u-mb0">出庫までに作業が不要</label>
                 </div>
                 <button class="c-button__register --gray u-mt0 u-mb0">検索</button>
               </div>
@@ -35,21 +35,37 @@
           </div>
 
           <button type="button" class="
-          c-button__register is-block u-horizontal-auto u-mt2 u-font--normal button_select">新規登録</button>
+          c-button__register is-block u-horizontal-auto u-mt2 u-font--normal button_select" onclick="openCreateModal()">新規登録</button>
         </form>
 
         <h2 class="c-title__lv2">登録済みカテゴリー一覧</h2>
         <table class="l-table-list">
           <thead class="l-table-list__head l-table-list--category_setting__head">
             <tr>
-              <th><div class="c-button-sort --asc">カテゴリー名</div></th>
+              <th><div class="c-button-sort sort-enable --asc">カテゴリー名</div></th>
               <th><div class="c-button-sort">区分</div></th>
               <th>メモ</th>
               <th> </th>
             </tr>
           </thead>
           <tbody class="l-table-list__body">
-            <tr>
+            @foreach ($goodCategories as $goodCategory)
+              <tr>
+                <td>{{$goodCategory->name}}</td>
+                <td>{{\App\Enums\GoodCategoryType::tryFrom($goodCategory->type)?->label()}}</td>
+                <td>{{$goodCategory->memo}}</td>
+                <td>
+                  <button class="c-button__edit button_select" onclick="openEditModal({{$goodCategory->id}})">編集</button>
+                </td>
+              </tr>
+            @endforeach
+            {{--  <tr>
+              <td>洗車</td>
+              <td>出庫までに作業が必要</td>
+              <td>MEMOメモめも</td>
+              <td><button class="c-button__edit button_select">編集</button></td>
+            </tr>  --}}
+            {{--  <tr>
               <td>洗車</td>
               <td>出庫までに作業が必要</td>
               <td>MEMOメモめも</td>
@@ -78,21 +94,16 @@
               <td>出庫までに作業が必要</td>
               <td>MEMOメモめも</td>
               <td><button class="c-button__edit button_select">編集</button></td>
-            </tr>
-            <tr>
-              <td>洗車</td>
-              <td>出庫までに作業が必要</td>
-              <td>MEMOメモめも</td>
-              <td><button class="c-button__edit button_select">編集</button></td>
-            </tr>
+            </tr>  --}}
           </tbody>
         </table>
       </div>
     </main>
 
   </div>
+
   <!-- 「編集」をクリックしたら出てくるmodal -->
-  <div id="modalAreaOption" class="l-modal">
+  {{--  <div id="modalAreaOption" class="l-modal isd-active">
     <!-- モーダルのinnerを記述   -->
     <div class="l-modal__inner l-modal--trash">
       <div class="l-modal__head">編集</div>
@@ -108,7 +119,7 @@
                 <dt>商品カテゴリー名</dt>
                 <dd>
                   <input type="text" class="u-w-full-wide">
-                </dd>          
+                </dd>
               </dl>
               <dl>
                 <dt>
@@ -125,7 +136,7 @@
                 <dt>メモ</dt>
                 <dd>
                   <textarea class="u-w-full-wide" rows="5"></textarea>
-                </dd>          
+                </dd>
               </dl>
 
           </div>
@@ -142,10 +153,10 @@
       </div>
     </div><!-- ./l-modal inner -->
     <!-- 閉じる・追加ボタン -->
-  </div>
+  </div>  --}}
 @endsection
 @push("scripts")
-  <script src="{{ asset('js/modalOption.js') }}"></script>
+  {{--  <script src="{{ asset('js/modalOption.js') }}"></script>  --}}
   <script src="{{ asset('js/tableHeaderSort.js') }}"></script>
   <!-- 閉じるボタン -->
   <script src="{{ asset('js/close_button_toggle.js') }}"></script>
