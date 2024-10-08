746961798762dd473fb7976793f332bec5f13dce
yasunoh <checkupmaster@gmail.com>
2024-10-08 17:57:19 +0900

    indexアクションに検索絞込追加。

--- app/Http/Controllers/Manage/Master/GoodCategoriesController.php
+++ app/Http/Controllers/Manage/Master/GoodCategoriesController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers\Manage\Master;
 
 use App\Http\Controllers\Manage\Controller;
+use App\Models\GoodCategory;
 use Illuminate\Http\Request;
 
 class GoodCategoriesController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
-        return view('manage.master.good_categories');
+        $goodCategories = GoodCategory::where('office_id', config('const.commons.office_id'))
+            ->when($request->input('name'), function($query, $search){
+                $query->where('name', 'LIKE', '%' . $search . '%');
+            })
+            ->when($request->input('type'), function($query, $search){
+                $query->where('type', $search);
+            })->orderBy('name')->get();
+
+
+        return view('manage.master.good_categories', [
+            'goodCategories' => $goodCategories,
+        ]);
     }
 }
