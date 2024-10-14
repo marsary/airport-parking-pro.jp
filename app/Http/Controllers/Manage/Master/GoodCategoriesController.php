6f5135a4a423cb7fe2627a2910a7931618e02a42
checkupmaster <checkupmaster@gmail.com>
2024-10-14 17:55:20 +0900

    登録・更新・削除アクション作成

--- app/Http/Controllers/Manage/Master/GoodCategoriesController.php
+++ app/Http/Controllers/Manage/Master/GoodCategoriesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Manage\Master;
 
+use App\Enums\RegiDisplayFlag;
 use App\Http\Controllers\Manage\Controller;
+use App\Http\Requests\Manage\Master\GoodCategoryRequest;
 use App\Models\GoodCategory;
 use Illuminate\Http\Request;
 
@@ -23,5 +25,41 @@ class GoodCategoriesController extends Controller
         return view('manage.master.good_categories', [
             'goodCategories' => $goodCategories,
         ]);
+    }
+
+
+    public function store(GoodCategoryRequest $request)
+    {
+        $goodCategory = GoodCategory::create([
+            'office_id' => config('const.commons.office_id'),
+            'name' => $request->name,
+            'type' => $request->type,
+            'regi_display_flag' => RegiDisplayFlag::REGI->value, // TODO デフォルトはどれか？
+            'memo' => $request->memo,
+        ]);
+
+        return redirect()->back();
+    }
+
+
+    public function update(GoodCategoryRequest $request, $id)
+    {
+        $goodCategory = GoodCategory::findOrFail($id);
+        $goodCategory->fill([
+            'name' => $request->name,
+            'type' => $request->type,
+            'memo' => $request->memo,
+        ])->save();
+
+        return redirect()->back();
+    }
+
+
+    public function destroy(Request $request, $id)
+    {
+        $goodCategory = GoodCategory::findOrFail($id);
+        $goodCategory->delete();
+
+        return redirect()->back();
     }
 }
