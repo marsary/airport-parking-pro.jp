768d8080a585789e5773d6b3b1ab0a28e7278fa9
yasunoh <checkupmaster@gmail.com>
2024-10-28 17:51:46 +0900

    agencyテーブル・モデルの更新

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Agency extends Model
 {
@@ -16,6 +18,7 @@ class Agency extends Model
      * @var array<int, string>
      */
     protected $fillable = [
+        'office_id',
         'name',
         'code',
         'zip',
@@ -23,6 +26,7 @@ class Agency extends Model
         'address2',
         'tel',
         'keyword',
+        'branch',
         'department',
         'position',
         'person',
@@ -31,7 +35,23 @@ class Agency extends Model
         'payment_destination',
         'memo',
         'receipt_issue',
+        'monthly_fixed_cost_flag',
+        'monthly_fixed_cost',
+        'incentive_flag',
+        'incentive',
+        'banner_comment_set',
+        'title_set',
+        'logo_image',
+        'campaign_image',
     ];
 
+    public function address(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value, $attributes) {
+                return $attributes['address1'] . $attributes['address2'];
 
+            }
+        );
+    }
 }
