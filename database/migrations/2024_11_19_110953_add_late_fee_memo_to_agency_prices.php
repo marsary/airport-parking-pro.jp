a6d8b61dcf271fb30105c6b3eaae6f8d07767532
yasunoh <checkupmaster@gmail.com>
2024-11-12 17:56:54 +0900

    テーブルにカラム追加

---
+++ database/migrations/2024_11_19_110953_add_late_fee_memo_to_agency_prices.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('agency_prices', function (Blueprint $table) {
+            $table->integer('late_fee')->nullable()->after('price_per_day');
+            $table->text('memo')->nullable()->after('no_discount_flg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('agency_prices', function (Blueprint $table) {
+            $table->dropColumn('late_fee');
+            $table->dropColumn('memo');
+        });
+    }
+};
