5652de434e608dba85a85f2a3e7d93da4177b69b
yasunoh <checkupmaster@gmail.com>
2024-11-26 17:44:37 +0900

    画面テンプレート修正

--- resources/views/manage/master/basic_prices.blade.php
+++ resources/views/manage/master/basic_prices.blade.php
@@ -9,6 +9,8 @@
         </li>
       </ul>
 
+      @include('include.messages.errors')
+
       <div class="l-container__inner">
         <div class="u-mb1">
           <h2 class="c-title__lv2 l-flex--sb">登録済み 適用料金表一覧<span class="close_button c-button__close">閉じる</span></h2>
@@ -23,7 +25,19 @@
                 </tr>
               </thead>
               <tbody class="l-table-list__body">
-                <tr>
+                @foreach ($prices as $price)
+                  <tr>
+                    <td>
+                      {{$price->start_date->format("Y/m/d")}} ～<br />{{$price->end_date->format("Y/m/d")}}
+                      {{--  2021/01/01～2021/12/31  --}}
+                    </td>
+                    <td>{{$price->memo}}</td>
+                    <td>
+                      <button type="button" class="c-button__edit button_select" onclick="openEditModal({{$price->id}})">編集</button>
+                    </td>
+                  </tr>
+                @endforeach
+                {{--  <tr>
                   <td>2021/01/01～2021/12/31</td>
                   <td>メモメモメモ</td>
                   <td>
@@ -43,15 +57,16 @@
                   <td>
                     <button type="button" class="c-button__edit button_select">編集</button>
                   </td>
-                </tr>
+                </tr>  --}}
               </tbody>
             </table>
           </div>
         </div>
 
-        <button type="" class="button_select c-button__register u-mb4 u-horizontal-auto">新規登録</button>
+        <button type="button" class="button_select c-button__register u-mb4 u-horizontal-auto" onclick="openCreateModal()">新規登録</button>
 
-        <form action="" method="POST" class="l-grid__right-submitButton u-mb4">
+        <form action="{{route('manage.master.prices.carsize_rate')}}" method="POST" class="l-grid__right-submitButton u-mb4">
+          @csrf
           <!-- 入力フォーム -->
           <div class="c-form__input">
             <!-- 下段 -->
@@ -63,26 +78,26 @@
                   <!-- 二輪 -->
                   <label class="l-flex--start l-grid--center l-grid--gap1 u-mb2">二輪
                     <div class="l-flex--start l-grid--gap05">
-                      ×<input type="text" name="two_wheeler" class="u-mb0">倍
+                      ×<input type="text" name="moter_cycle" class="u-mb0" value="{{old('moter_cycle', \Illuminate\Support\Arr::get($carSizeMap, 'moter_cycle') )}}">倍
                     </div>
                   </label>
                   <!-- 大型 -->
                   <label class="l-flex--start l-grid--center l-grid--gap1">大型
                     <div class="l-flex--start l-grid--gap05">
-                      ×<input type="text" name="large_size" class="u-mb0">倍
+                      ×<input type="text" name="large" class="u-mb0" value="{{old('large', \Illuminate\Support\Arr::get($carSizeMap, 'large') )}}">倍
                     </div>
                 </div>
                 <div>
                   <!-- マイクロバス -->
                   <label class="l-flex--end l-grid--center l-grid--gap1 u-mb2">マイクロバス
                     <div class="l-flex--start l-grid--gap05">
-                      ×<input type="text" id="micro_bus" name="micro_bus" class="u-mb0">倍
+                      ×<input type="text" id="micro_bus" name="micro_bus" class="u-mb0" value="{{old('micro_bus', \Illuminate\Support\Arr::get($carSizeMap, 'micro_bus') )}}">倍
                     </div>
                   </label>
                   <!-- キャンピングカー -->
                   <label class="l-flex--end l-grid--center l-grid--gap1">キャンピングカー
                     <div class="l-flex--start l-grid--gap05">
-                      ×<input type="text" id="camping_car" name="camping_car" class="u-mb0">倍
+                      ×<input type="text" id="camping_car" name="camping_car" class="u-mb0" value="{{old('camping_car', \Illuminate\Support\Arr::get($carSizeMap, 'camping_car') )}}">倍
                     </div>
                   </label>
                 </div>
@@ -99,142 +114,69 @@
     </main><!-- /.l-container__main -->
   </div><!-- /.l-wrap -->
 
-  <!-- 閉じる・開く切替 -->
-  <script src="{{ asset('js/close_button_toggle.js') }}"></script>
 
   <!-- modal -->
-  <div id="modalAreaOption" class="l-modal">
-    <!-- モーダルのinnerを記述   -->
-    <div class="l-modal__inner l-modal--trash">
-      <div class="l-modal__head">編集</div>
-      <!-- close button -->
-      <div class="l-modal__close modal_optionClose">×</div>
-      <div class="l-modal__content">
-        <form action="" method="POST" class="l-flex--column l-flex--sb u-w-full">
-          <!-- 入力フォーム -->
-          <div class="u-w-full-wide">
-            <div class="c-title__modal--lv3">基本料金設定編集</div>
-            <div class="">
-              <div class="l-grid--col2 l-grid--gap2">
-                <div>
-                  <label for="price_list" class="u-font--md">料金表選択</label>
-                  <div class="c-form-select-wrap">
-                    <select name="price_list" id="price_list">
-                      <option value="" disabled selected>選択してください</option>
-                      <option value="price_list_New">新規登録</option>
-                      <option value="price_list1">2024年1月1日～2024年3月31日</option>
-                      <option value="price_list2">2024年4月28日～2024年5月8日</option>
-                    </select>
-                  </div>
-                </div>
+  @include('manage.master.components.price_modal', [
+    'mode' => 'new',
+    'label' => '新規登録',
+    'method' => 'POST',
+    'action' => route('manage.master.prices.store'),
+    'price' => null,
+    ]
+  )
 
-                <!-- 料金の適用期間 -->
-                <div>
-                  <p class="u-mb025 u-font--md">料金の適用期間</p>
-                  <div class="l-flex l-grid--gap05">
-                    <input type="date" id="start_date" name="start_date" class="u-w-full-wide">
-                    <p class="u-mb1">～</p>
-                    <input type="date" id="end_date" name="end_date" class="u-w-full-wide">
-                  </div>
-                </div>
-              </div>
+  <!-- 「編集」をクリックしたら出てくるmodal -->
+  @foreach ($prices as $price)
+    @include('manage.master.components.price_modal', [
+      'mode' => 'edit',
+      'label' => '編集',
+      'method' => 'PUT',
+      'action' => route('manage.master.prices.update', [$price->id]),
+      'price' => $price,
+      ]
+    )
+  @endforeach
 
-              <!-- MEMO -->
-              <label for="memo" class="u-font--md">メモ</label>
-              <textarea name="memo" id="memo" rows="1" class="u-w-full-wide"></textarea>
-
-              <!-- 1日目～15日まで料金を入力するinput -->
-              <div class="l-grid--pricing l-grid--input">
-                <div>
-                  <label for="day1">1日目</label>
-                  <input type="text" id="day1" name="day1">
-                </div>
-                <div>
-                  <label for="day2">2日目</label>
-                  <input type="text" id="day2" name="day2">
-                </div>
-                <div>
-                  <label for="day3">3日目</label>
-                  <input type="text" id="day3" name="day3">
-                </div>
-                <div>
-                  <label for="day4">4日目</label>
-                  <input type="text" id="day4" name="day4">
-                </div>
-                <div>
-                  <label for="day5">5日目</label>
-                  <input type="text" id="day5" name="day5">
-                </div>
-                <div>
-                  <label for="day6">6日目</label>
-                  <input type="text" id="day6" name="day6">
-                </div>
-                <div>
-                  <label for="day7">7日目</label>
-                  <input type="text" id="day7" name="day7">
-                </div>
-                <div>
-                  <label for="day8">8日目</label>
-                  <input type="text" id="day8" name="day8">
-                </div>
-                <div>
-                  <label for="day9">9日目</label>
-                  <input type="text" id="day9" name="day9">
-                </div>
-                <div>
-                  <label for="day10">10日目</label>
-                  <input type="text" id="day10" name="day10">
-                </div>
-                <div>
-                  <label for="day11">11日目</label>
-                  <input type="text" id="day11" name="day11">
-                </div>
-                <div>
-                  <label for="day12">12日目</label>
-                  <input type="text" id="day12" name="day12">
-                </div>
-                <div>
-                  <label for="day13">13日目</label>
-                  <input type="text" id="day13" name="day13">
-                </div>
-                <div>
-                  <label for="day14">14日目</label>
-                  <input type="text" id="day14" name="day14">
-                </div>
-                <div>
-                  <label for="day15">15日目</label>
-                  <input type="text" id="day15" name="day15">
-                </div>
-                <div>
-                  <label for="day16">16日目以降</label>
-                  <input type="text" id="day16" name="day16">
-                </div>
-                <div>
-                  <label for="extensionFee">延長料</label>
-                  <input type="text" id="extensionFee" name="extensionFee">
-                </div>
-                <div class="--last">
-                  <label for="basicFee">基本料金</label>
-                  <input type="text" id="basicFee" name="basicFee">
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 閉じる・追加ボタン -->
-          <div class="l-flex--center l-grid--gap1 u-mt2 u-mb4">
-            <button type="submit" class="c-button__submit--dark-gray modal_optionClose">閉じる</button>
-            <button type="submit" class="c-button__submit">登録</button>
-          </div>
-        </form>
-      </div><!-- ./l-modal__content -->
-
-      <!-- 編集の場合のデータ削除ボタン -->
-      <div class="l-modal__trashButton">
-        <img src="{{asset('images/svg/trash.svg')}}" alt="ゴミ箱" width="100%" class="l-modal--trashButton">
-      </div>
-    </div><!-- ./l-modal inner -->
-  </div>
-
-  <!-- モーダル -->
-  <script src="{{ asset('js/modalOption.js') }}"></script>
 @endsection
+@push("scripts")
+<style>
+  .carsize-price-rate-inputs{
+    column-gap: 20%;
+  }
+</style>
+@endpush
+@push("scripts")
+  <!-- 閉じる・開く切替 -->
+  <script src="{{ asset('js/close_button_toggle.js') }}"></script>
+  <!-- モーダル -->
+  {{--  <script src="{{ asset('js/modalOption.js') }}"></script>  --}}
+
+  <script>
+    let createModal;
+    let modalAreaOptions;
+    let modalCloseOption;
+
+    function openCreateModal() {
+      createModal.classList.add('is-active');
+    }
+    function openEditModal(priceId) {
+      document.getElementById(`modalAreaOption_edit_${priceId}`).classList.add('is-active');
+    }
+    function closeCreateModal() {
+      createModal.classList.remove('is-active');
+    }
+    function closeEditModal(priceId) {
+      document.getElementById(`modalAreaOption_edit_${priceId}`).classList.remove('is-active');
+    }
+    function deletePrice(priceId) {
+      document.getElementById(`delete_${priceId}_form`).submit();
+    }
+
+    window.addEventListener('DOMContentLoaded', function() {
+      createModal = document.getElementById('modalAreaOption_new_');
+      modalAreaOptions = document.querySelectorAll('.modal_area');
+      modalCloseOption = document.querySelectorAll('.modal_optionClose');
+    })
+
+  </script>
+@endpush
