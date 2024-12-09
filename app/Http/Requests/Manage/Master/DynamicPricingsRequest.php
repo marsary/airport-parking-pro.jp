0bcf01569fb274670ef5b27b4fbda6d58ac4162b
yasunoh <checkupmaster@gmail.com>
2024-12-09 17:34:22 +0900

    DynamicPricingsRequest変更

--- app/Http/Requests/Manage/Master/DynamicPricingsRequest.php
+++ app/Http/Requests/Manage/Master/DynamicPricingsRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DynamicPricingsRequest extends FormRequest
 {
+    private $recordKey;
+
     /**
      * Determine if the user is authorized to make this request.
      */
@@ -21,43 +23,49 @@ class DynamicPricingsRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => 'required|string|max:255',
-            'p10' => 'nullable|int',
-            'p20' => 'nullable|int',
-            'p30' => 'nullable|int',
-            'p40' => 'nullable|int',
-            'p50' => 'nullable|int',
-            'p60' => 'nullable|int',
-            'p70' => 'nullable|int',
-            'p80' => 'nullable|int',
-            'p90' => 'nullable|int',
-            'p100' => 'nullable|int',
-            'p110' => 'nullable|int',
-            'p120' => 'nullable|int',
-            'p130' => 'nullable|int',
-            'p131' => 'nullable|int',
+        // 動的なレコードIDに基づいた入力データの取得
+        $this->recordKey = "record_" . $this->route()->parameter('agency_price', 0);
+        $rules = [
+            'sort' => 'required|int',
+            "{$this->recordKey}.name" => 'required|string|max:255',
+            "{$this->recordKey}.p10" => 'nullable|int',
+            "{$this->recordKey}.p20" => 'nullable|int',
+            "{$this->recordKey}.p30" => 'nullable|int',
+            "{$this->recordKey}.p40" => 'nullable|int',
+            "{$this->recordKey}.p50" => 'nullable|int',
+            "{$this->recordKey}.p60" => 'nullable|int',
+            "{$this->recordKey}.p70" => 'nullable|int',
+            "{$this->recordKey}.p80" => 'nullable|int',
+            "{$this->recordKey}.p90" => 'nullable|int',
+            "{$this->recordKey}.p100" => 'nullable|int',
+            "{$this->recordKey}.p110" => 'nullable|int',
+            "{$this->recordKey}.p120" => 'nullable|int',
+            "{$this->recordKey}.p130" => 'nullable|int',
+            "{$this->recordKey}.p131" => 'nullable|int',
         ];
+
+        return $rules;
     }
+
 
     public function attributes()
     {
         return [
-            'name' => '名前',
-            'p10' => '0-10%',
-            'p20' => '11-20%',
-            'p30' => '21-30%',
-            'p40' => '31-40%',
-            'p50' => '41-50%',
-            'p60' => '51-60%',
-            'p70' => '61-70%',
-            'p80' => '71-80%',
-            'p90' => '81-90%',
-            'p100' => '91-100%',
-            'p110' => '101-110%',
-            'p120' => '111-120%',
-            'p130' => '121-130%',
-            'p131' => '131%-',
+            "{$this->recordKey}.name" => '名前',
+            "{$this->recordKey}.p10" => '0-10%',
+            "{$this->recordKey}.p20" => '11-20%',
+            "{$this->recordKey}.p30" => '21-30%',
+            "{$this->recordKey}.p40" => '31-40%',
+            "{$this->recordKey}.p50" => '41-50%',
+            "{$this->recordKey}.p60" => '51-60%',
+            "{$this->recordKey}.p70" => '61-70%',
+            "{$this->recordKey}.p80" => '71-80%',
+            "{$this->recordKey}.p90" => '81-90%',
+            "{$this->recordKey}.p100" => '91-100%',
+            "{$this->recordKey}.p110" => '101-110%',
+            "{$this->recordKey}.p120" => '111-120%',
+            "{$this->recordKey}.p130" => '121-130%',
+            "{$this->recordKey}.p131" => '131%-',
         ];
     }
 }
