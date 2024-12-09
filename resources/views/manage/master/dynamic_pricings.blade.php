f4eb24264d7b9c041522a13c32c5a196078c0f49
yasunoh <checkupmaster@gmail.com>
2024-12-09 17:37:34 +0900

    テンプレートの更新

--- resources/views/manage/master/dynamic_pricings.blade.php
+++ resources/views/manage/master/dynamic_pricings.blade.php
@@ -1,257 +1,264 @@
 <!-- ダイナミックプライシング 料金表一覧 -->
 @extends('layouts.manage.authenticated')
 
-@section('content')    <main class="l-wrap__main l-container__main">
+@section('content')
+  <div class="l-wrap">
     <main class="l-wrap__main l-container__main">
       <!-- パンくず -->
       <ul class="l-wrap__breadcrumb l-breadcrumb">
         <li class="l-breadcrumb__list">ダイナミックプライシング料金表一覧</li>
-        </li>
       </ul>
 
+      @include('include.messages.errors')
+
+
+
       <div class="l-container__inner">
-       <p>設定した在庫数に対しての割合</p>
-        
-      <table class="l-table-dynamic-pricing-list">
-        <thead>
-          <tr>
-            <th> </th>
-            <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
-            <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
-            <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
-            <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
-            <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
-            <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>閑散期</th>
-            <th>週末</th>
-            <th>年末年始</th>
-            <th>DP料金4</th>
-            <th>DP料金5</th>
-            <th>DP料金6</th>
-          </tr>
-          <tr>
-            <th>0-10%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>11%-20%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>21%-30%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>31%-40%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>41%-50%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>51%-60%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>61%-70%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>71%-80%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>81%-90%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>91%-100%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>101%-110%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>111%-120%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>121%-130%</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <th>131%～</th>
-            <td>100</td>
-            <td>500</td>
-            <td>2000</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-        </tbody>
-      </table>
+        <p>設定した在庫数に対しての割合</p>
+
+        <table class="l-table-dynamic-pricing-list">
+          <thead>
+            <tr>
+              <th> </th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <th><button type="button" class="c-button__edit l-table-dynamic-pricing-list__button button_select" onclick="openEditModal({{$dynamicPricing->sort}})">編集</button></th>
+              @endforeach
+              {{--  <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
+              <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
+              <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
+              <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
+              <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>
+              <th><button type="" class="c-button__edit l-table-dynamic-pricing-list__button button_select">編集</button></th>  --}}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th></th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                  <th>{{ $dynamicPricing->name }}</th>
+              @endforeach
+              {{--  <th>閑散期</th>
+              <th>週末</th>
+              <th>年末年始</th>
+              <th>DP料金4</th>
+              <th>DP料金5</th>
+              <th>DP料金6</th>  --}}
+            </tr>
+            <tr>
+              <th>0-10%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p10 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>11%-20%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p20 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>21%-30%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p30 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>31%-40%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p40 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>41%-50%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p50 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>51%-60%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p60 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>61%-70%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p70 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>71%-80%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p80 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>81%-90%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p90 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>91%-100%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p100 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>101%-110%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p110 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>111%-120%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p120 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>121%-130%</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p130 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+            <tr>
+              <th>131%～</th>
+              @foreach ($dynamicPricings as $dynamicPricing)
+                <td>{{ $dynamicPricing->p131 }}</td>
+              @endforeach
+              {{--  <td>100</td>
+              <td>500</td>
+              <td>2000</td>
+              <td>0</td>
+              <td>0</td>
+              <td>0</td>  --}}
+            </tr>
+          </tbody>
+        </table>
 
       </div>
     </main><!-- /.l-container__main -->
   </div><!-- /.l-wrap -->
 
   <!-- 編集ボタンを押したら出てくるmodal -->
-  <!-- 「編集」をクリックしたら出てくるmodal -->
-  <div id="modalAreaOption" class="l-modal">
-    <!-- モーダルのinnerを記述   -->
-    <div class="l-modal__inner l-modal--trash">
-      <div class="l-modal__head">編集</div>
-      <!-- close button -->
-      <div class="l-modal__close modal_optionClose">×</div>
-      <div class="l-modal__content">
-        <form action="" method="POST" class="l-grid--pricing-modal__form">
-          <div class="l-grid--pricing-modal__box u-mb2 u-mt3">
-            <label for="name">名称</label>
-            <input type="text" id="name" name="" class="u-w-full-wide">
-          </div>
-          <div class="l-grid--pricing-modal__inner">
-            <div class="l-grid--pricing-modal__box">
-              <label for="">0-10%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">11%-20%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">21%-30%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">31%-40%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">41%-50%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">51%-60%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">61%-70%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">71%-80%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">81%-90%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">91%-100%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">101%-110%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">111%-120%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">121%-130%</label>
-              <input type="text" id="" name="">
-            </div>
-            <div class="l-grid--pricing-modal__box">
-              <label for="">131%-</label>
-              <input type="text" id="" name="">
-            </div>
-          </div>
-     
-          <!-- 登録ボタン -->
-          <div class="l-flex--center l-grid--gap1 u-mt4">
-            <button type="button" class="c-button__submit--dark-gray modal_optionClose">閉じる</button>
-            <button type="submit" class="c-button__submit">保存</button>
-          </div>
-        </form>
-      </div><!-- ./l-modal__content -->
+  @foreach ($dynamicPricings as $dynamicPricing)
+    @if ($dynamicPricing->id)
+      @include('manage.master.components.dynamic_pricings_modal', [
+        'mode' => 'edit',
+        'label' => '編集',
+        'method' => 'PUT',
+        'action' => route('manage.master.dynamic_pricings.update', [$dynamicPricing->id]),
+        'dynamicPricing' => $dynamicPricing,
+        ]
+      )
+    @else
+      @include('manage.master.components.dynamic_pricings_modal', [
+        'mode' => 'edit',
+        'label' => '編集',
+        'method' => 'POST',
+        'action' => route('manage.master.dynamic_pricings.store'),
+        'dynamicPricing' => $dynamicPricing,
+        ]
+      )
+    @endif
+  @endforeach
 
-    </div><!-- ./l-modal inner -->
-    <!-- 閉じる・追加ボタン -->
-  </div>
 
-  <!-- モーダル -->
-  <script src="{{ asset('js/modalOption.js') }}"></script>
 @endsection
+@push("scripts")
+<!-- モーダル -->
+{{--  <script src="{{ asset('js/modalOption.js') }}"></script>  --}}
+<script>
+  let modalCloseOption;
+  let sortInput;
+  function openEditModal(dynamicPricingId) {
+    document.getElementById(`modalAreaOption_edit_${dynamicPricingId}`).classList.add('is-active');
+  }
+  function closeEditModal(dynamicPricingId) {
+    document.getElementById(`modalAreaOption_edit_${dynamicPricingId}`).classList.remove('is-active');
+  }
+
+  window.addEventListener('DOMContentLoaded', function() {
+    modalCloseOption = document.querySelectorAll('.modal_optionClose');
+  })
+</script>
+@endpush
