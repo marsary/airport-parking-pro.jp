a838845bfc55f39dd133d83bcf3b2314fd15db13
yasunoh <checkupmaster@gmail.com>
2025-01-20 17:55:38 +0900

    ルーティング追加

--- routes/member_auth.php
+++ routes/member_auth.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Member\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 
 // 顧客用のルーティング
 
@@ -11,6 +13,19 @@ Route::middleware('guest')->group(function () {
                 ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+        ->name('password.store');
+
 });
 
 Route::middleware('auth:members')->group(function () {
