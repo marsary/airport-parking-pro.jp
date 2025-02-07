90d253fcdd9729bf0ad647c404af898e9b438dbf
Spice Inc. <113090065+spice-web@users.noreply.github.com>
2025-02-07 11:54:52 +0900

    Update index.blade.php

--- resources/views/manage/master/index.blade.php
+++ resources/views/manage/master/index.blade.php
@@ -5,20 +5,20 @@
 
 @section('content')
 <main class="l-wrap__main">
-    <!-- パンくず -->
-    <ul class="l-wrap__breadcrumb l-breadcrumb">
-        <li class="l-breadcrumb__list">マスタ設定一覧</li>
-    </ul>
+  <!-- パンくず -->
+  <ul class="l-wrap__breadcrumb l-breadcrumb">
+    <li class="l-breadcrumb__list">マスタ設定一覧</li>
+  </ul>
 
-    <div class="l-container__inner">
+      <div class="l-container__inner">
         <div class="l-grid--col5-1fr l-grid--rgap2 l-grid--cgap1 u-font--white">
-            <a href="/manage/master/good_categories" class="c-button--yellow c-link-no-border">商品カテゴリーマスタ</a>
-            <a href="/manage/master/goods" class="c-button--yellow c-link-no-border">商品マスタ</a>
-            <a href="/manage/master/agencies" class="c-button--yellow c-link-no-border">代理店マスタ</a>
-            <a href="/manage/master/coupons" class="c-button--yellow c-link-no-border">クーポンマスタ</a>
+          <a href="/manage/master/good_categories" class="c-button--yellow c-link-no-border">商品カテゴリーマスタ</a>
+          <a href="/manage/master/goods" class="c-button--yellow c-link-no-border">商品マスタ</a>
+          <a href="/manage/master/agencies" class="c-button--yellow c-link-no-border">代理店マスタ</a>
+          <a href="/manage/master/coupons" class="c-button--yellow c-link-no-border">クーポンマスタ</a>
         </div>
-    </div><!-- ./l-container__inner -->
-</main>
+      </div><!-- ./l-container__inner -->
+    </main>
 
 </div>
 @endsection
