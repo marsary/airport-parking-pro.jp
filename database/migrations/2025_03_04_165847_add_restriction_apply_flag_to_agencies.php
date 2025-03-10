fce31c903ffffcfff585f3839da4c94b156dfe5e
yasunoh <checkupmaster@gmail.com>
2025-03-10 17:55:20 +0900

    restriction_apply_flagカラムを追加

---
+++ database/migrations/2025_03_04_165847_add_restriction_apply_flag_to_agencies.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('agencies', function (Blueprint $table) {
+            $table->boolean('restriction_apply_flag')->nullable()->after('receipt_issue');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('agencies', function (Blueprint $table) {
+            $table->dropColumn('restriction_apply_flag');
+        });
+    }
+};
