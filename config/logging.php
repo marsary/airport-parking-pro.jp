d50c84e8e6797f078a03a989eacd05862909c858
yasunoh <checkupmaster@gmail.com>
2025-04-14 17:40:04 +0900

    update logging.php

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,13 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'remind_mail' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/emails/remind_mail.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => env('LOG_DAILY_DAYS', 14),
+            'replace_placeholders' => true,
+        ],
     ],
 
 ];
