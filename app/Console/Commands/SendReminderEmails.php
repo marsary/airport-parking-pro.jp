1a7a59176f0d86665648f1c24b0952a7ac3ff805
yasunoh <checkupmaster@gmail.com>
2025-04-14 17:39:46 +0900

    update SendReminderEmails

--- app/Console/Commands/SendReminderEmails.php
+++ app/Console/Commands/SendReminderEmails.php
@@ -17,7 +17,7 @@ class SendReminderEmails extends Command
      *
      * @var string
      */
-    protected $signature = 'app:send-reminder-emailss';
+    protected $signature = 'app:send-reminder-emails {--test}';
 
     /**
      * The console command description.
@@ -31,11 +31,17 @@ class SendReminderEmails extends Command
      */
     public function handle()
     {
+        $isTest = $this->option('test');
         // 1. 開始ログ出力
+        $this->info('リマインドメール送信バッチ処理を開始します');
         Log::channel('remind_mail')->info('リマインドメール送信バッチ処理を開始します');
 
         try {
             $query = Deal::query();
+            if ($isTest) {
+                $this->info('テストモードで実行します。');
+                $query->where('remarks', 'like', '%テスト用%');
+            }
 
             // 2. 取引テーブルからデータを取得
             $deals = $query->where('remind_mail_sent_flg', false)
@@ -47,6 +53,9 @@ class SendReminderEmails extends Command
             // 3. 各取引に対してメール送信処理
             foreach ($deals as $deal) {
                 try {
+                    if(isBlank($deal->email)) {
+                        throw new ErrorException('この取引にはメールアドレスが設定されていません。');
+                    }
                     // メール送信
                     Mail::to($deal->email)->send(new ReminderEmail($deal));
 
@@ -54,15 +63,18 @@ class SendReminderEmails extends Command
                     $deal->remind_mail_sent_flg = true;
                     $deal->save();
 
+                    Log::channel('remind_mail')->info('メール送信成功 - 取引ID: ' . $deal->id);
                 } catch (\Exception $e) {
-                    Log::channel('remind_mail')->error('メール送信失敗 - エラー: ' . $e->getMessage());
+                    Log::channel('remind_mail')->error('メール送信失敗 - 取引ID: ' . $deal->id . ' - エラー: ' . $e->getMessage());
                 }
             }
 
             // 5. 終了ログ出力
             Log::channel('remind_mail')->info('リマインドメール送信バッチ処理を終了します');
-        } catch (\Exception $e) {
-            Log::channel('remind_mail')->error('バッチ処理でエラーが発生しました: ' . $e->getMessage());
+            $this->info('リマインドメール送信バッチ処理を終了します');
+        } catch (\Throwable $th) {
+            Log::channel('remind_mail')->error('バッチ処理でエラーが発生しました: ' . $th->getMessage());
+            $this->error('バッチ処理でエラーが発生しました: ' . $th->getMessage());
         }
     }
 }
