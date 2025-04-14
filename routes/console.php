0b1e5cc679266024f2e5dced15951c44867b6c06
yasunoh <checkupmaster@gmail.com>
2025-04-14 17:40:11 +0900

    update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Console\Commands\SendReminderEmails;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+// テストモード
+// Schedule::command(SendReminderEmails::class, ['--test'])->hourly();
+// 本番モード
+Schedule::command(SendReminderEmails::class)->hourly();
