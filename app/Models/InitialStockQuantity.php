fe529e2417fb86b9f5a6c52fb09acf1683af22eb
yasunoh <checkupmaster@gmail.com>
2025-04-15 18:02:55 +0900

    モデルの更新

---
+++ app/Models/InitialStockQuantity.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class InitialStockQuantity extends Model
+{
+    use HasFactory, SoftDeletes;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'start_date',
+        'starting_quantity',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'start_date' => 'date',
+        ];
+    }
+
+}
---
+++ app/Models/LoadUnloadInventoryPerformance.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LoadUnloadInventoryPerformance extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'target_date',
+        'load_quantity',
+        'unload_quantity',
+        'stock_quantity',
+    ];
+
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'target_date' => 'date',
+        ];
+    }
+}
