bad6fa0c62970b6d7ce1de2690c636d8460c0101
yasunoh <checkupmaster@gmail.com>
2025-05-12 17:28:15 +0900

    CalcDailyInventory 完成

--- app/Console/Commands/CalcDailyInventory.php
+++ app/Console/Commands/CalcDailyInventory.php
@@ -55,15 +55,14 @@ class CalcDailyInventory extends Command
             $prevStockQuantity = $inventory->stock_quantity;
 
             // システム日付前日までの処理
-            while($targetDate < $sysDate->system_date) {
-                $this->calcAndSaveDailyInventory($targetDate, $prevStockQuantity);
-            }
+            $this->calcAndSaveDailyInventoryBeforeSysDate($targetDate, $prevStockQuantity);
+
 
             // 以下、対象日付から180日後まで繰り返し行う。
             $endDate = $targetDate->copy()->addDays(180);
 
             while($targetDate <= $endDate) {
-                $this->calcAndSaveDailyInventory($targetDate, $prevStockQuantity);
+                $this->calcAndSaveDailyInventoryAfterSysDate($targetDate, $prevStockQuantity);
             }
 
 
@@ -83,21 +82,14 @@ class CalcDailyInventory extends Command
         $inventory = LoadUnloadInventoryPerformance::whereDate('target_date', $targetDate->copy()->subDays(1))
             ->first();
 
+        // データがなかった場合はエラー
         if(!$inventory) {
-            $inventory = LoadUnloadInventoryPerformance::whereDate('target_date', '<', $targetDate->copy()->subDays(1))
-                ->orderBy('target_date', 'desc')
-                ->first();
-
-            if(!$inventory) {
-                throw new ErrorException('入出庫在庫実績からデータを取得できませんでした。');
-            } else {
-                $targetDate = $inventory->target_date->addDay();
-            }
+            throw new ErrorException('入出庫在庫実績からデータを取得できませんでした。');
         }
         return $inventory;
     }
 
-    private function calcAndSaveDailyInventory(Carbon $targetDate, &$prevStockQuantity)
+    private function calcAndSaveDailyInventoryBeforeSysDate(Carbon $targetDate, &$prevStockQuantity)
     {
         // 対象日付の入庫実績を取得する。
         $loadCount = Deal::whereDate('load_date', $targetDate)
@@ -126,13 +118,48 @@ class CalcDailyInventory extends Command
         $targetDate->addDay();
     }
 
+    private function calcAndSaveDailyInventoryAfterSysDate(Carbon $targetDate, &$prevStockQuantity)
+    {
+        // 対象日付の入庫実績を取得する。
+        $loadCount = Deal::whereDate('load_date', $targetDate)
+            ->where('transaction_type', TransactionType::PARKING->value)
+            ->count();
+
+        // 対象日付（出庫予定日が対象日付）の出庫実績を取得する。
+        $unloadCount = Deal::whereDate('unload_date_plan', $targetDate)
+            ->where('status', DealStatus::UNLOADED->value)
+            ->where('transaction_type', TransactionType::PARKING->value)
+            ->count();
+
+        // 当日在庫数＝前日在庫数＋当日入庫数ー当日出庫数
+        $stockQuantity = $prevStockQuantity + $loadCount - $unloadCount;
+        // 入出庫在庫実績にデータを保存する
+        $this->saveInventory($stockQuantity, $loadCount, $unloadCount, $targetDate);
+
+        // 在庫数を前日在庫数とする
+        $prevStockQuantity = $stockQuantity;
+
+        // 対象日付を１日進める
+        $targetDate->addDay();
+    }
+
     private function saveInventory(int $stockQuantity, int $loadCount, int $unloadCount, Carbon $targetDate)
     {
-        LoadUnloadInventoryPerformance::create([
-            'target_date' => $targetDate,
-            'load_quantity' => $loadCount,
-            'unload_quantity' => $unloadCount,
-            'stock_quantity' => $stockQuantity,
-        ]);
+        $inventory = LoadUnloadInventoryPerformance::whereDate('target_date', $targetDate)
+            ->first();
+
+        if($inventory) {
+            $inventory->load_quantity = $loadCount;
+            $inventory->unload_quantity = $unloadCount;
+            $inventory->stock_quantity = $stockQuantity;
+            $inventory->save();
+        } else {
+            LoadUnloadInventoryPerformance::create([
+                'target_date' => $targetDate,
+                'load_quantity' => $loadCount,
+                'unload_quantity' => $unloadCount,
+                'stock_quantity' => $stockQuantity,
+            ]);
+        }
     }
 }
