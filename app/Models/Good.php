51dc557aebc1e3385a8a9c57f1a1a671651b294b
miuraryosuke <miura.marsary@gmail.com>
2025-05-30 01:02:34 +0900

    元に戻す

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use App\Models\GoodCategory;
 
 class Good extends Model
 {
