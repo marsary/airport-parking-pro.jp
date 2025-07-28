3224a191b6515ff982fffc187707c7fd4e3219c7
yasunoh <checkupmaster@gmail.com>
2025-07-28 17:25:16 +0900

    RegiChecklistsRequest

---
+++ app/Http/Requests/Manage/Ledger/RegiChecklistsRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Manage\Ledger;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegiChecklistsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'register' => 'nullable|int',
+            'entry_date' => 'nullable|date',
+            'entry_time' => [
+                'nullable',
+                'string',
+                function (string $attribute, mixed $value, \Closure $fail) {
+                    if ($value === 'all') {
+                        return;
+                    }
+                    $d = \DateTime::createFromFormat('H:i', $value);
+                    if (!($d && $d->format('H:i') === $value)) {
+                        $fail('時刻は H:i 形式、または "all" を指定してください。');
+                    }
+                },
+            ],
+        ];
+    }
+
+
+
+    public function attributes()
+    {
+        return [
+            'register' => 'レジ番号',
+            'entry_date' => '日付',
+            'entry_time' => '時刻',
+        ];
+    }
+}
