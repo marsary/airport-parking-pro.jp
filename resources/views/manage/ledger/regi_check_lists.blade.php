479e4a8d4fc8c9509c5ac10620c9a08be5ee4f53
yasunoh <checkupmaster@gmail.com>
2025-07-29 17:48:46 +0900

    テンプレートの更新

--- resources/views/manage/ledger/regi_check_lists.blade.php
+++ resources/views/manage/ledger/regi_check_lists.blade.php
@@ -16,7 +16,14 @@
             レジ番号
             <!-- チェックボックス レジ番号 -->
             <div class="l-flex l-grid--gap2">
-              <label class="l-flex l-grid--gap05">
+              @foreach ($registers as $register)
+                <label class="l-flex l-grid--gap05">
+                  <input class="u-mb0" type="radio" name="register" value="{{$register->id}}"
+                     {{request('register') == $register->id ? "checked":""}}
+                  >レジ{{str_pad($register->id, 2, '0', STR_PAD_LEFT)}}
+                </label>
+              @endforeach
+              {{--  <label class="l-flex l-grid--gap05">
                 <input class="u-mb0" type="radio" name="register" value="1">レジ01
               </label>
               <label class="l-flex l-grid--gap05">
@@ -30,7 +37,7 @@
               </label>
               <label class="l-flex l-grid--gap05">
                 <input class="u-mb0" type="radio" name="register" value="5">レジ05
-              </label>
+              </label>  --}}
             </div>
           </div>
 
@@ -39,37 +46,37 @@
               <!-- 日付  -->
               <div class="l-flex--column l-flex--item-start l-grid--gap025">
                 <label for="entry_date" class="l-flex l-grid--gap05">日付</label>
-                <input type="date" id="entry_date" name="entry_date" placeholder="例) 2021/01/01">
+                <input type="date" id="entry_date" name="entry_date" placeholder="例) 2021/01/01" value="{{request('entry_date')}}">
               </div>
               <!-- 時刻  -->
               <div class="l-flex--column l-flex--item-start l-grid--gap025">
-                <label for="entry_date" class="l-flex l-grid--gap05">時刻</label>
-                <select name="" id="">
+                <label for="entry_time" class="l-flex l-grid--gap05">時刻</label>
+                <select name="entry_time" id="entry_time">
                   <option value="all" selected>全て</option>
-                  <option value="00:00">00:00</option>
-                  <option value="01:00">01:00</option>
-                  <option value="02:00">02:00</option>
-                  <option value="03:00">03:00</option>
-                  <option value="04:00">04:00</option>
-                  <option value="05:00">05:00</option>
-                  <option value="06:00">06:00</option>
-                  <option value="07:00">07:00</option>
-                  <option value="08:00">08:00</option>
-                  <option value="09:00">09:00</option>
-                  <option value="10:00">10:00</option>
-                  <option value="11:00">11:00</option>
-                  <option value="12:00">12:00</option>
-                  <option value="13:00">13:00</option>
-                  <option value="14:00">14:00</option>
-                  <option value="15:00">15:00</option>
-                  <option value="16:00">16:00</option>
-                  <option value="17:00">17:00</option>
-                  <option value="18:00">18:00</option>
-                  <option value="19:00">19:00</option>
-                  <option value="20:00">20:00</option>
-                  <option value="21:00">21:00</option>
-                  <option value="22:00">22:00</option>
-                  <option value="23:00">23:00</option>
+                  <option value="00:00" {{request('entry_time')=="00:00" ? 'selected':''}}>00:00</option>
+                  <option value="01:00" {{request('entry_time')=="01:00" ? 'selected':''}}>01:00</option>
+                  <option value="02:00" {{request('entry_time')=="02:00" ? 'selected':''}}>02:00</option>
+                  <option value="03:00" {{request('entry_time')=="03:00" ? 'selected':''}}>03:00</option>
+                  <option value="04:00" {{request('entry_time')=="04:00" ? 'selected':''}}>04:00</option>
+                  <option value="05:00" {{request('entry_time')=="05:00" ? 'selected':''}}>05:00</option>
+                  <option value="06:00" {{request('entry_time')=="06:00" ? 'selected':''}}>06:00</option>
+                  <option value="07:00" {{request('entry_time')=="07:00" ? 'selected':''}}>07:00</option>
+                  <option value="08:00" {{request('entry_time')=="08:00" ? 'selected':''}}>08:00</option>
+                  <option value="09:00" {{request('entry_time')=="09:00" ? 'selected':''}}>09:00</option>
+                  <option value="10:00" {{request('entry_time')=="10:00" ? 'selected':''}}>10:00</option>
+                  <option value="11:00" {{request('entry_time')=="11:00" ? 'selected':''}}>11:00</option>
+                  <option value="12:00" {{request('entry_time')=="12:00" ? 'selected':''}}>12:00</option>
+                  <option value="13:00" {{request('entry_time')=="13:00" ? 'selected':''}}>13:00</option>
+                  <option value="14:00" {{request('entry_time')=="14:00" ? 'selected':''}}>14:00</option>
+                  <option value="15:00" {{request('entry_time')=="15:00" ? 'selected':''}}>15:00</option>
+                  <option value="16:00" {{request('entry_time')=="16:00" ? 'selected':''}}>16:00</option>
+                  <option value="17:00" {{request('entry_time')=="17:00" ? 'selected':''}}>17:00</option>
+                  <option value="18:00" {{request('entry_time')=="18:00" ? 'selected':''}}>18:00</option>
+                  <option value="19:00" {{request('entry_time')=="19:00" ? 'selected':''}}>19:00</option>
+                  <option value="20:00" {{request('entry_time')=="20:00" ? 'selected':''}}>20:00</option>
+                  <option value="21:00" {{request('entry_time')=="21:00" ? 'selected':''}}>21:00</option>
+                  <option value="22:00" {{request('entry_time')=="22:00" ? 'selected':''}}>22:00</option>
+                  <option value="23:00" {{request('entry_time')=="23:00" ? 'selected':''}}>23:00</option>
                 </select>
               </div>
             </div>
@@ -86,7 +93,7 @@
             <button id="printButton" class="link-white" onclick="window.print()">印刷</button>
           </li>
           <!-- 点検表へのリンクURL未設定 -->
-          <li class="c-button--deep-gray"><a href="" class="link-white">売上帳へ</a></li>
+          <li class="c-button--deep-gray"><a href="{{route('manage.ledger.regi_sales_account_books')}}" class="link-white">売上帳へ</a></li>
         </ul>
         <div class="l-table-print__wrap l-print__wrap">
           <p>【売上 成田】</p>
