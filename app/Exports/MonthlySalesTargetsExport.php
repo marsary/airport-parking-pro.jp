7346f6abb4184648926e22dc2317ab4b36e95965
yasunoh <checkupmaster@gmail.com>
2025-11-11 17:12:30 +0900

    MonthlySalesTargetsExport修正

--- app/Exports/MonthlySalesTargetsExport.php
+++ app/Exports/MonthlySalesTargetsExport.php
@@ -6,14 +6,15 @@ use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class MonthlySalesTargetsExport implements FromArray, WithHeadings
+class MonthlySalesTargetsExport implements FromArray, WithHeadings, WithMapping
 {
     use Exportable;
 
     /** @var array<array<string, mixed>> */
     protected $csvData;
-    /** @var int */
+
     private int $year;
 
     public function __construct(array $csvData)
@@ -80,58 +81,4 @@ class MonthlySalesTargetsExport implements FromArray, WithHeadings
             '商品カテゴリー4売上目標',
         ];
     }
-
-    // public function generator(): \Generator
-    // {
-    //     $tableTitles = [
-    //         'total_sales' => '月間総売上',
-    //         'parking_fee' => '駐車料金',
-    //         'good_category_1' => '商品カテゴリー1 月間総売上',
-    //         'good_category_2' => '商品カテゴリー2 月間総売上',
-    //         'good_category_3' => '商品カテゴリー3 月間総売上',
-    //         'good_category_4' => '商品カテゴリー4 月間総売上',
-    //     ];
-
-    //     $rowTitles = [
-    //         'target' => '売上目標',
-    //         'result' => '売上実績',
-    //         'difference' => '目標と実績の差額',
-    //         'achievement_rate' => '売上目標に対する達成率',
-    //     ];
-
-    //     $isFirstTable = true;
-
-    //     foreach ($tableTitles as $tableKey => $tableTitle) {
-    //         if (!isset($this->tableData[$tableKey])) {
-    //             continue;
-    //         }
-
-    //         if (!$isFirstTable) {
-    //             // テーブル間に一行開ける
-    //             yield [''];
-    //         }
-    //         $isFirstTable = false;
-
-    //         // 1. テーブル見出し行
-    //         $headerRow = [$tableTitle];
-    //         for ($month = 1; $month <= 12; $month++) {
-    //             $headerRow[] = "{$this->year}年{$month}月";
-    //         }
-    //         yield $headerRow;
-
-    //         // 2. データ行
-    //         foreach ($rowTitles as $rowKey => $rowTitle) {
-    //             $dataRow = [$rowTitle];
-    //             for ($month = 1; $month <= 12; $month++) {
-    //                 $value = $this->tableData[$tableKey][$rowKey][$month] ?? '';
-    //                 if ($rowKey === 'achievement_rate' && is_numeric($value)) {
-    //                     $dataRow[] = $value . '%';
-    //                 } else {
-    //                     $dataRow[] = $value;
-    //                 }
-    //             }
-    //             yield $dataRow;
-    //         }
-    //     }
-    // }
 }
