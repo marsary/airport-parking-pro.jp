92e9a8d1368c98f5f55f4137d28cf3ed91db1a5c
yasunoh <checkupmaster@gmail.com>
2026-06-08 18:06:08 +0900

    EntryDateRequest更新

---
+++ app/Http/Requests/Form/EntryDateRequest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Requests\Form;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\DB;
+
+class EntryDateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'agency_code' => 'nullable|string|exists:agencies,code|max:100',
+            'agency_id' => 'nullable|int',
+            'load_date' => 'required|date',
+            'load_time' => 'required|date_format:G:i',
+            'unload_date_plan' => 'required|date|after_or_equal:load_date',
+            'unload_time_plan' => 'nullable|date_format:H:i',
+            'num_days' => 'required|int',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'load_date.required' => '入庫日が設定されていません。',
+            'unload_date_plan.required'  => '出庫日が設定されていません。',
+            'load_time.required'  => '入庫時間が設定されていません。',
+            'unload_date_plan.after_or_equal'  => '出庫日は入庫日以降の日付を設定してください。',
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        if(isset($this->agency_code)) {
+            $agency = DB::table('agencies')->where('code', $this->agency_code)->first();
+        }
+        $this->merge([
+            'agency_id' => isset($agency) ? $agency->id: null,
+        ]);
+
+        if ($this->unload_time_plan) {
+            try {
+                $this->merge([
+                    'unload_time_plan' => Carbon::parse($this->unload_time_plan)->format('H:i'),
+                ]);
+            } catch (\Throwable $th) {
+                // パースに失敗した場合はバリデーションに任せる
+            }
+        }
+
+        if ($this->load_time) {
+            try {
+                $this->merge([
+                    'load_time' => Carbon::parse($this->load_time)->format('G:i'),
+                ]);
+            } catch (\Throwable $th) {
+                // パースに失敗した場合はバリデーションに任せる
+            }
+        }
+    }
+
+    public function attributes()
+    {
+        return [
+            'agency_code' => '代理店ID',
+            'load_date' => '入庫日',
+            'load_time' => '入庫時間',
+            'unload_date_plan' => '出庫予定日',
+            'unload_time_plan' => '出庫予定時間',
+            'num_days' => '利用日数',
+        ];
+    }
+}
