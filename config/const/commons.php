12c4f4b906828af4a383e9221d377de3c21dc130
yasunoh <checkupmaster@gmail.com>
2026-06-19 18:01:00 +0900

    commons.php項目追加

--- config/const/commons.php
+++ config/const/commons.php
@@ -2,6 +2,7 @@
 
 return [
     'office_id' => env('OFFICE_ID', 1),
+    'form_office_id' => 1,
     // 予約カレンダーの選択できる日付は5か月先まで
     'reserve_cal_month_periods' => env('RESERVE_CAL_MONTH_PERIODS', 5),
     // 予約開始可能日
