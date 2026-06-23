60572d013e0e2a184712de1464cb08a9b552552b
yasunoh <checkupmaster@gmail.com>
2026-06-23 17:43:47 +0900

    ReserveFormの修正

--- app/Http/Controllers/Form/Forms/ReserveForm.php
+++ app/Http/Controllers/Form/Forms/ReserveForm.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Http\Controllers\Form\Forms;
 
+use App\Enums\DealStatus;
 use App\Http\Controllers\Forms\ReserveFormBase;
 use App\Models\Member;
+use App\Models\MemberCar;
+use Carbon\Carbon;
 use Illuminate\Support\Str;
 
 class ReserveForm extends ReserveFormBase
@@ -10,9 +13,37 @@ class ReserveForm extends ReserveFormBase
     public bool $insurance = false;
     public bool $carwash = false;
 
+
+    function __construct()
+    {
+        $this->office_id = config('const.commons.form_office_id');
+        $this->status = DealStatus::NOT_LOADED->value;
+        $this->reserve_date = Carbon::now();
+        $this->reserve_code = Str::ulid();
+    }
+
     public function setMember(?Member $member = null)
     {
-        parent::setMember($member);
+        if($member === null) {
+            return;
+        }
+        $this->member_id = $member->id;
+        $this->member = $member;
+        $this->fill($member);
+
+        $memberCar = MemberCar::with('car.carMaker')->where('member_id', $member->id)
+            ->where('office_id', config('const.commons.form_office_id'))
+            ->orderBy('default_flg', 'desc')
+            ->orderBy('updated_at', 'desc')
+            ->first();
+
+        if($memberCar) {
+            $this->car_maker_id = $memberCar->car?->carMaker->id;
+            $this->car_id = $memberCar->car_id;
+            $this->car_color_id = $memberCar->car_color_id;
+            $this->car_number = $memberCar->number;
+            $this->member_car_id = $memberCar->id;
+        }
     }
 
     public function fillMember()
@@ -20,7 +51,7 @@ class ReserveForm extends ReserveFormBase
         if(!$this->member) {
             $this->member = new Member();
             $this->member->member_code = Str::ulid();
-            $this->member->office_id = $this->office_id;
+            $this->member->office_id = config('const.commons.form_office_id');
         }
         $this->member->fill([
             'name' => $this->name,
