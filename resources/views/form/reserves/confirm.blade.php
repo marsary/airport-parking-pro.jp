e20ec1b2afb9a1f8340b8d3f709462f3f2d0cf04
miuraryosuke <miura.marsary@gmail.com>
2026-07-04 19:28:39 +0900

    修正

--- resources/views/form/reserves/confirm.blade.php
+++ resources/views/form/reserves/confirm.blade.php
@@ -192,7 +192,7 @@
         </tr>
         @if (!empty($reserve->season_price))
           <tr>
-            <th>駐車料金</th>
+            <th>シーズン料金</th>
             <td class="u-font-nowrap">{{number_format($reserve->season_price)}}円</td>
             <td class="--tax">({{ $reserve->getTaxTypeLabel(\App\Enums\TaxType::TEN_PERCENT->value) }})</td>
           </tr>
