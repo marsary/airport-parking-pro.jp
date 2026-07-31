54471fcab5bad527119030706a2b327ea351a368
yasunoh <checkupmaster@gmail.com>
2026-07-31 17:55:00 +0900

    FullLimitDateService作成

---
+++ app/Services/FormCalendar/FullLimitDateService.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Services\FormCalendar;
+
+use App\Enums\LimitOverStatus;
+use App\Models\FullLimitSetting;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
+use Illuminate\Support\Collection;
+
+class FullLimitDateService
+{
+    /**
+     * @var Collection<string, FullLimitSetting>
+     */
+    private Collection $dateLimits;
+    private CarbonPeriod $period;
+
+
+    public function __construct(
+        Carbon $startDate,
+        Carbon $endDate
+    ) {
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->period = CarbonPeriod::create($startDate, $endDate);
+
+        $this->dateLimits = FullLimitSetting::query()
+            ->where('office_id', config('const.commons.office_id'))
+            ->whereBetween('target_date', [
+                $startDate->toDateString(),
+                $endDate->toDateString(),
+            ])
+            ->get()
+            ->keyBy(function (FullLimitSetting $item) {
+                return Carbon::parse($item->target_date)->toDateString();
+            });
+    }
+
+    /**
+     * 指定日の制限情報を取得
+     */
+    public function getDateLimit(Carbon $date): array
+    {
+        /** @var FullLimitSetting|null $limit */
+        $limit = $this->dateLimits->get($date->toDateString());
+
+        if (!$limit) {
+            return [
+                'load_date' => LimitOverStatus::VACANT->label(),
+                'unload_date' => LimitOverStatus::VACANT->label(),
+                'cross_time' => LimitOverStatus::VACANT->label(),
+                'canCheckIn' => true,
+                'canCheckOut' => true,
+                'canCrossTime' => true,
+            ];
+        }
+
+        $load = $limit->limitOverStatus(FullLimitSetting::LOAD_LIMIT_SYMBOL);
+        $unload = $limit->limitOverStatus(FullLimitSetting::UNLOAD_LIMIT_SYMBOL);
+        $cross = $limit->limitOverStatus(FullLimitSetting::CROSS_TIME_SYMBOL);
+
+        return [
+            'load_date' => $load->label(),
+            'unload_date' => $unload->label(),
+            'cross_time' => $cross->label(),
+            'canCheckIn' => $load !== LimitOverStatus::FULL,
+            'canCheckOut' => $unload !== LimitOverStatus::FULL,
+            'canCrossTime' => $cross !== LimitOverStatus::FULL,
+        ];
+    }
+
+    /**
+     * カレンダー表示用データ
+     */
+    public function getCalendar(): array
+    {
+        $results = [];
+
+        foreach ($this->period as $date) {
+            $results[$date->toDateString()] = $this->getDateLimit($date);
+        }
+
+        return $results;
+    }
+
+}
